e7a1a3842edaef9d53f81c8ca20f1ac726c4e8fe
ysyx_22050853 Huang Junzhe <425331389@qq.com>
2022-12-06 13:16:59 +0800

    NJU-ProjectN/nemu ysyx2204 initialized
    NJU-ProjectN/nemu 09bb925782871ed4eb98d17cfabab323d473df62 riscv32,inst: refactor INSTPAT

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-STUID = ysyx_22040000
-STUNAME = 张三
+STUID = ysyx_22050853
+STUNAME = 黄俊哲
 
 # DO NOT modify the following code!!!
 
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,36 @@
+#include "Vtop.h" // .v文件的名字为name，则为Vname.h
+#include "verilated.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated_vcd_c.h" // 波形仿真
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+
+int main(int argc, char** argv, char** env) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+	Vtop* top = new Vtop{contextp};
+	// 生成波形
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	contextp->traceEverOn(true);
+	top->trace(tfp, 0);
+	tfp->open("./obj_dir/wave.vcd");
+
+	while (!contextp->gotFinish()) {
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+
+		assert(top->f == (a ^ b));
+	}
+	delete top;
+	tfp->close();
+	delete contextp;
+	return 0;
 }
