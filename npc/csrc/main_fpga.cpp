2f4baf2e29149906fc9c40e212dcd9af2e2fb8e6
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-07 00:14:37 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:14:37 up 11:08,  5 users,  load average: 0.06, 0.05, 0.01

---
+++ npc/csrc/main_fpga.cpp
@@ -0,0 +1,20 @@
+#include "Vtop.h" // .v文件的名字为name，则为Vname.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <nvboard.h> // nvboard
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top); 
+
+int main(int argc, char** argv, char** env) {
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+	while (1) {
+		nvboard_update();
+	}
+
+	nvboard_quit();
+	return 0;
+}
