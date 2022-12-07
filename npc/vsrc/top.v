ebf82ed3d9b30dd8514ee0d0f09735fb10713505
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-07 17:31:18 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:31:18 up 20:11,  5 users,  load average: 0.26, 0.22, 0.10

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,9 +1,26 @@
-module top(
-	input a,
-	input b,
-	output f
+module (
+	input clk,
+	input rst,
+	output reg [15:0] led
 );
 
-assign f = a ^ b;
+reg [31:0] count;
 
+always @(posedge clk) begin
+	if (rst)
+		count <= 0;
+	else if (count == 5000000)
+		count <= 0;
+	else
+		count <= count + 1;
+end
+
+always @(posedge clk) begin
+	if (rst)
+		led <= 1;
+	else if (count == 5000000)
+		led <= (led == 16'h8000) ? 16'd1 : led << 1;
+	else
+		led <= led;	
+end
 endmodule
