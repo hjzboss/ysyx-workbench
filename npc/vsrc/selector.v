72b69a62dc79b3a2133aca8424a71b4e23766547
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-08 10:42:17 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:42:17 up 23:08,  5 users,  load average: 0.20, 0.23, 0.09

---
+++ npc/vsrc/selector.v
@@ -0,0 +1,19 @@
+module top(
+	input [1:0] y,
+	input [1:0] x0,
+	input [1:0] x1,
+	input [1:0] x2,
+	input [1:0] x3,
+	output reg [1:0] f
+);
+
+always @(*) begin
+	case (y)
+		2'b00: f = x0;
+		2'b01: f = x1;
+		2'b10: f = x2;
+		2'b11: f = x3;
+		default: f = 0;
+	endcase
+end
+endmodule
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,19 +1,34 @@
-module top(
-	input [1:0] y,
-	input [1:0] x0,
-	input [1:0] x1,
-	input [1:0] x2,
-	input [1:0] x3,
-	output reg [1:0] f
+module top (
+	input					en,
+	input [7:0]		sw,
+	
+	output				valid,
+	output [7:0]	led,
+
+	output [6:0]	seg0
 );
 
-always @(*) begin
-	case (y)
-		2'b00: f = x0;
-		2'b01: f = x1;
-		2'b10: f = x2;
-		2'b11: f = x3;
-		default: f = 0;
+localparam ZERO = 7'b0000001, ONE = 7'b1001111, TWO = 7'b0010010, THREE = 7'b0000110, FOUR = 7'b1001100;
+localparam FIVE = 7'b0100100, SIX = 7'b0100000, SEVEN = 7'b0001111;
+
+reg [6:0] tmp;
+
+assign led = en ? sw : 0;
+assign seg0 = en ? tmp : ZERO;
+assign valid = sw != 8'd0;
+
+always @(*) begin	
+	casez (sw)
+		8'b00000000:	tmp = ZERO;
+		8'b00000001:	tmp = ZERO;
+		8'b0000001?:	tmp = ONE;
+		8'b000001??:	tmp = TWO;
+		8'b00001???:	tmp = THREE;
+		8'b0001????:	tmp = FOUR;
+		8'b001?????:	tmp = FIVE;
+		8'b01??????:	tmp = SIX;
+		8'b1???????:	tmp = SEVEN;	
+		default:			tmp = ZERO;
 	endcase
 end
 endmodule
