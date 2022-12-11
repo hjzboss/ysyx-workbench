ab42a3d0c11cdc733f4adfa1b9461878016bb391
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-11 21:04:47 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:04:47 up 1 day, 15:59,  5 users,  load average: 0.16, 0.40, 0.29

--- npc/csrc/main_fpga.cpp
+++ npc/csrc/main_fpga.cpp
@@ -7,45 +7,31 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top); 
 
+/*
 static void single_cycle() {
 	dut.clock = 0;
 	dut.eval();
-	unsigned long n = 1844674407370955161;
-	for (unsigned long i = n; i > 0; --i) {
-		for (unsigned long j = n; j > 0; --j) {
-			for (unsigned long k = n; k > 0; --k) {
-				for (unsigned long w = n; w > 0; --w) {}
-			}
-		}
-	}
 	dut.clock = 1;
 	dut.eval();
-	for (unsigned long i = n; i > 0; --i) {
-		for (unsigned long j = n; j > 0; --j) {
-			for (unsigned long k = n; k > 0; --k) {
-				for (unsigned long w = n; w > 0; --w) {}
-			}
-		}
-	}
 }
 
 static void reset(int n) {
 	dut.reset = 1;
-	while (n--)
-		single_cycle();
+	single_cycle();
 	dut.reset = 0;
 }
+*/
 
 int main(int argc, char** argv, char** env) {
 	nvboard_bind_all_pins(&dut);
 	nvboard_init();
 	
-	reset(10);
+	//reset(10);
 
 	while (1) {
-		//dut.eval();
+		dut.eval();
 		nvboard_update();
-		single_cycle();
+		//single_cycle();
 	}
 
 	nvboard_quit();
