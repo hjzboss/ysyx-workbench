08dc753af5d2956c78c01d20fd477ca118c18a2a
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-14 21:47:30 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:47:30 up 3 days, 13:48,  5 users,  load average: 0.14, 0.14, 0.08

--- npc/vsrc/vmem.v
+++ npc/vsrc/vmem.v
@@ -70,8 +70,8 @@ assign tmp_col = h_addr - ({3'd0,x} << 3 + {3'd0,x});
 assign row = tmp_row[3:0];
 assign col = tmp_col[3:0];
 
-always @(x) begin
-	$display("%d", x);
+always @(col) begin
+	$display("%d", col);
 end
 
 endmodule
