9050dcb2dfe0924fa148b575207a112aac485953
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-14 20:55:07 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:55:07 up 3 days, 12:55,  5 users,  load average: 0.08, 0.15, 0.13

--- npc/vsrc/rom.v
+++ npc/vsrc/rom.v
@@ -7,11 +7,13 @@ module rom(
 
 reg [11:0] mem [0:4095];
 wire [11:0] row_addr;
+wire [3:0] col_addr;
 
 initial begin
 	$readmemh("/home/hjz/ysyx-workbench/npc/resource/vga_font.txt", mem);
 end
 
+assign col_addr = 4'd12 - col;
 assign row_addr = {4'd0, ascii_in} << 4 + {8'd0, row};
 assign data = mem[row_addr][col];
 endmodule
