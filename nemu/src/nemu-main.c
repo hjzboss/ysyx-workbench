070baadc2f3a5cf99bbcf4a5a019887a57409008
tracer-ysyx2204 <tracer@ysyx.org>
2022-12-30 22:03:34 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:03:34 up 6 days,  9:09,  9 users,  load average: 0.09, 0.12, 0.04

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-	/*
+	
 	// test eval
 	char *src = "/home/hjz/ysyx-workbench/nemu/tools/gen-expr/input";
 	FILE *fp = fopen(src, "r");
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	fclose(fp);
-	*/
+
   /* Start engine. */
   engine_start();
 
