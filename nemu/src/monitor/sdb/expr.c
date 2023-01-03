6696aa0372279289f365feb4f3afaf4ad4a17150
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-03 21:52:37 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:52:37 up 6 days, 20:36,  9 users,  load average: 0.35, 0.11, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -37,7 +37,7 @@ static struct rule {
    )*/
 
   {" +", TK_NOTYPE},																						// spaces
-  {"(?<=[\\d\\(\\)]( )*)\\+(?=( )*[\\d\\(\\)])", PLUS},					// plus
+  {"(?<=[\\d\\(\\)] *)\\+(?= *[\\d\\(\\)])", PLUS},					// plus
   {"==", TK_EQ},																								// equal
 	{"[0-9]+", INTEGER},																					// integer
 	{"(?<=[\\d\\(\\)]( )*)-(?=( )*[\\d\\(\\)])", MINUS},					// minus
