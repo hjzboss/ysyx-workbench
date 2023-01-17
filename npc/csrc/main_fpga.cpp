59a370904579fe42f55176be5285a47ea6febd4a
ysyx_22050853 Huang Junzhe <425331389@qq.com>
2023-01-18 00:34:38 +0800

    修改makefile

---
+++ npc/csrc/main_fpga.cpp
@@ -0,0 +1,53 @@
+#include "Vtop.h" // .v文件的名字为name，则为Vname.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <nvboard.h> // nvboard
+
+static TOP_NAME dut;
+
+void single_cycle() {
+	dut.clock = 0;
+	dut.eval();
+	/*
+	unsigned long n = 4294967295;
+	for (unsigned long i = n; i > 0; --i) {
+		for (unsigned long j = n; j > 0; --j) {
+			for (unsigned long k = n; k > 0; --k) {}
+		}
+	}*/
+	dut.clock = 1;
+	dut.eval();	
+	/*
+	for (unsigned long i = n; i > 0; --i) {
+		for (unsigned long j = n; j > 0; --j) {
+			for (unsigned long k = n; k > 0; --k) {}
+		}
+	}*/
+	
+}
+
+
+void reset(int n) {
+	dut.reset = 1;
+	while (n--) {
+		single_cycle();
+	}
+	dut.reset = 0;
+}
+
+void nvboard_bind_all_pins(Vtop* top); 
+
+int main(int argc, char** argv, char** env) {
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+
+	reset(10);
+
+	while (1) {
+		single_cycle();
+		nvboard_update();
+	}
+
+	nvboard_quit();
+	return 0;
+}
---
+++ npc/csrc/main_sim.cpp
@@ -0,0 +1,32 @@
+#include "Vtop.h" // .v文件的名字为name，则为Vname.h
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "verilated_vcd_c.h" // 波形仿真
+
+int main(int argc, char** argv, char** env) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc, argv);
+	Vtop* top = new Vtop{contextp};
+	// 生成波形
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	contextp->traceEverOn(true);
+	top->trace(tfp, 99);
+	tfp->open("./build/sim/obj_dir/wave.vcd");
+
+	while (!contextp->gotFinish()) {
+		int en = rand() & 0x1;
+		int sw = rand() & 0xff;
+		top->en = en;
+		top->sw = sw;
+		top->eval();
+		//printf("en = %o, sw = %o\n", top->en, top->sw, top->valid, top->led, top->seg0); 
+		// 推进仿真时间
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+	}
+	delete top;
+	tfp->close();
+	delete contextp;
+	return 0;
+}
