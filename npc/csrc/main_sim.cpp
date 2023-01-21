cc142b1b26a3c8b69ebdbea20cb2a5812ba9a3cf
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 23:48:14 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:48:14 up 1 day,  2:40,  1 user,  load average: 0.09, 0.08, 0.08

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -4,23 +4,31 @@
 #include <stdlib.h>
 #include "verilated_vcd_c.h" // 波形仿真
 
-uint32_t instr_cache[65535] = {};
+static uint32_t instr_cache[65535] = {};
 
-void init_cache() {
+static VJzCore* top;
+
+static void single_cycle() {
+  top->clock = 0; top->eval();
+  top->clock = 1; top->eval();
+}
+
+static void init_cache() {
   instr_cache[0] = 0x00138393;
   instr_cache[1] = 0x00238393;
   instr_cache[2] = 0x00338393;
 }
 
-uint32_t pmem_read(uint64_t pc) {
+static uint32_t pmem_read(uint64_t pc) {
   return instr_cache[pc];
 }
 
+void 
 
 int main(int argc, char** argv, char** env) {
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  VJzCore* top = new VJzCore{contextp};
+  top = new VJzCore{contextp};
   // 生成波形
   VerilatedVcdC* tfp = new VerilatedVcdC;
   contextp->traceEverOn(true);
@@ -28,13 +36,12 @@ int main(int argc, char** argv, char** env) {
   tfp->open("./build/sim/obj_dir/wave.vcd");
   
   init_cache();
-  while (contextp->time() < 30 && !contextp->gotFinish()) {
+  while (contextp->time() < 3 && !contextp->gotFinish()) {
     top->io_inst = pmem_read(top->io_pc);
-    top->eval();
+    single_cycle();
+
     //printf("en = %o, sw = %o\n", top->en, top->sw, top->valid, top->led, top->seg0); 
-    // 推进仿真时间
-    tfp->dump(contextp->time());
-    contextp->timeInc(1);
+
   }
 
   delete top;
