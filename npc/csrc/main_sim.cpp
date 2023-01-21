f5fc78713afd97dc4bd8a227144198f2bd8473c1
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-22 01:16:32 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:16:32 up 1 day,  4:09,  1 user,  load average: 0.10, 0.07, 0.08

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -1,17 +1,19 @@
-#include "VJzCore.h" // .v文件的名字为name，则为Vname.h
-#include "verilated.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "verilated_vcd_c.h" // 波形仿真
+#include <verilated.h>
+
+// Include model header, generated from Verilating "jzcore.v"
+#include "VJzCore.h"
+
+// If "verilator --trace" is used, include the tracing class
+#include <verilated_vcd_c.h>
+
+// Current simulation time (64-bit unsigned)
+vluint64_t main_time = 0;
+// Called by $time in Verilog
+double sc_time_stamp () {
+  return main_time; // Note does conversion to real, to match SystemC
+}
 
 static uint32_t instr_cache[65535] = {};
-
-static VJzCore* top;
-
-static void single_cycle() {
-  top->clock = 0; top->eval();
-  top->clock = 1; top->eval();
-}
 
 static void init_cache() {
   instr_cache[0] = 0x00138393;
@@ -24,30 +26,69 @@ static uint32_t pmem_read(uint64_t pc) {
 }
 
 
+#define MAX_SIM_TIME 100 //max simulation time
+
 int main(int argc, char** argv, char** env) {
-  VerilatedContext* contextp = new VerilatedContext;
-  contextp->commandArgs(argc, argv);
-  top = new VJzCore{contextp};
-  // 生成波形
-  VerilatedVcdC* tfp = new VerilatedVcdC;
-  contextp->traceEverOn(true);
-  top->trace(tfp, 99);
-  tfp->open("./build/sim/obj_dir/wave.vcd");
-  
+  // This is a more complicated example, please also see the simpler examples/hello_world_c.
+
+  // Prevent unused variable warnings
+  if (0 && argc && argv && env) {}
+  // Pass arguments so Verilated code can see them, e.g. $value$plusargs
+  Verilated::commandArgs(argc, argv);
+
+  // Set debug level, 0 is off, 9 is highest presently used
+  Verilated::debug(0);
+
+  // Randomization reset policy
+  //Verilated::randReset(2);
+
+  // Construct the Verilated model, from Vjzcore.h generated from Verilating "jzcore.v"
+  VJzCore* jzcore = new VJzCore; // Or use a const unique_ptr, or the VL_UNIQUE_PTR wrapper
+
+  VerilatedVcdC* tfp = NULL;
+  Verilated::traceEverOn(true);  // Verilator must compute traced signals
+  tfp = new VerilatedVcdC;
+  jzcore->trace(tfp, 99);  // Trace 99 levels of hierarchy
+  tfp->open("./build/sim/obj_dir/wave.vcd");  // Open the dump file
+
+  // initial memory
   init_cache();
 
+  // Set some inputs
+  jzcore->reset = 1;
 
-  while (contextp->time() < 30 && !contextp->gotFinish()) {
-    top->io_inst = pmem_read(top->io_pc);
-    //single_cycle();
-    //printf("en = %o, sw = %o\n", top->en, top->sw, top->valid, top->led, top->seg0); 
-    // 推进仿真时间
-    tfp->dump(contextp->time());
-    contextp->timeInc(1);
+  // Simulate until $finish
+  while (!Verilated::gotFinish() && (main_time <= MAX_SIM_TIME)) {
+    if (!jzcore->reset)
+      jzcore->io_inst = pmem_read(jzcore->io_pc);
+
+    // reset signal remains for 1000 ns(100 cycles)
+    if(main_time > 8){
+        jzcore->reset = 0;
+    }
+    if ((main_time % 10) == 1) { // 1 cycle is 10 ns
+        jzcore->clock = 1;
+    }
+    if ((main_time % 10) == 6) {
+        jzcore->clock = 0;
+    }
+
+    // Evaluate model
+    jzcore->eval();
+
+    tfp->dump(main_time);//dump wave
+    main_time++;  // Time passes...
   }
 
-  delete top;
-  tfp->close();
-  delete contextp;
-  return 0;
+  // Final model cleanup
+  jzcore->final();
+
+  // Close trace if opened
+  if (tfp) { tfp->close(); }
+
+  // Destroy model
+  delete jzcore; jzcore = NULL;
+
+  // Fin
+  exit(0);
 }
