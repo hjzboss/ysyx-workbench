ac6b813d8eff9ff28526ab13f28c9a1794b99eb3
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-21 23:32:50 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:32:50 up 1 day,  2:25,  1 user,  load average: 0.24, 0.10, 0.05

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -4,41 +4,43 @@
 #include <stdlib.h>
 #include "verilated_vcd_c.h" // 波形仿真
 
-uint32_t instr_cache[65535] = {}
+uint32_t instr_cache[65535] = {};
 
 void init_cache() {
-  instr_cache[0] = 0x
+  instr_cache[0] = 0x00138393;
+  instr_cache[1] = 0x00238393;
+  instr_cache[2] = 0x00338393;
 }
 
 uint32_t pmem_read(uint64_t pc) {
-
+  return instr_cache[pc];
 }
 
 
 int main(int argc, char** argv, char** env) {
-	VerilatedContext* contextp = new VerilatedContext;
-	contextp->commandArgs(argc, argv);
-	Vtop* top = new Vtop{contextp};
-	// 生成波形
-	VerilatedVcdC* tfp = new VerilatedVcdC;
-	contextp->traceEverOn(true);
-	top->trace(tfp, 99);
-	tfp->open("./build/sim/obj_dir/wave.vcd");
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+  // 生成波形
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 99);
+  tfp->open("./build/sim/obj_dir/wave.vcd");
+  int time = 0;
 
-	while (!contextp->gotFinish()) {
-		int en = rand() & 0x1;
-		int sw = rand() & 0xff;
-		top->en = en;
-		top->sw = sw;
-		top->eval();
-		//printf("en = %o, sw = %o\n", top->en, top->sw, top->valid, top->led, top->seg0); 
-		// 推进仿真时间
-		tfp->dump(contextp->time());
-		contextp->timeInc(1);
-	}
+  while (!contextp->gotFinish()) {
+    if (time == 3) break;
+    top->inst = pmem_read(top->pc)
+    top->eval();
+    //printf("en = %o, sw = %o\n", top->en, top->sw, top->valid, top->led, top->seg0); 
+    // 推进仿真时间
+    tfp->dump(contextp->time());
+    contextp->timeInc(1);
+    tim++;
+  }
 
-	delete top;
-	tfp->close();
-	delete contextp;
-	return 0;
+  delete top;
+  tfp->close();
+  delete contextp;
+  return 0;
 }
