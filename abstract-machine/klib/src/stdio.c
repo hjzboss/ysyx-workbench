3cba2bb1a787beb9a8e3e3af452c509b47b95a5b
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-24 19:57:46 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:57:46 up 2 days, 5 min,  1 user,  load average: 0.10, 0.10, 0.03

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,39 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+char* my_itoa(int value)
+{
+	int i = 0, k = 0, size = 0;;
+	static char string[16];
+	char* p = string;
+
+	// negative number to positive number
+	if(value >> 31)
+	{
+		value = ((~value) + 1);
+		*p++ = '-';
+	}
+
+	// integer to string
+	do
+	{
+		p[size++] = value % 10 + '0';
+		value /= 10;
+	}while(value > 0);
+
+	p[size] = '\0';
+
+	// reverse
+	for(i = 0, k = size - 1; i < k; i++, k--)
+	{
+		p[i] ^= p[k];
+		p[k] ^= p[i];
+		p[i] ^= p[k];
+	}
+
+	return string;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +47,43 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  char *s = out;
+  va_list ap;
+  char *p, *sval;
+  int ival;
+  size_t len;
+
+  size_t arg_cnt = 0;
+
+  va_start(ap, fmt);
+  for (p = (char *)fmt; *p; p++) {
+    if (*p != '%') {
+      *s++ = *p;
+      continue;
+    }
+    switch (*++p) {
+      case 'd':
+        ival = va_arg(ap, int);
+        char *str = my_itoa(ival);
+        len = strlen(str);
+        strcpy(s, str);
+        s += len;
+        arg_cnt += len;
+        break;
+      case 's':
+        sval = va_arg(ap, char*);
+        len = strlen(sval);
+        strcpy(s, sval);
+        s += len;
+        arg_cnt += len;
+        break;
+      default:
+        panic("Not implemented");
+    }
+  }
+  va_end(ap);
+
+  return arg_cnt;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
