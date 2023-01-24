36d1c45bb2979cf4ce7ae994018440abba9aa234
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-24 20:42:54 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:42:54 up 2 days, 50 min,  1 user,  load average: 0.05, 0.06, 0.07

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,33 +5,27 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char* my_itoa(int value)
-{
+char* my_itoa(int value) {
 	int i = 0, k = 0, size = 0;;
 	static char string[16];
 	char* p = string;
 
-/*
 	// negative number to positive number
-	if(value >> 31)
-	{
+	if(value >> 31) {
 		value = ((~value) + 1);
 		*p++ = '-';
 	}
-  */
 
 	// integer to string
-	do
-	{
+	do {
 		p[size++] = value % 10 + '0';
 		value /= 10;
-	}while(value > 0);
+	} while(value > 0);
 
 	p[size] = '\0';
 
 	// reverse
-	for(i = 0, k = size - 1; i < k; i++, k--)
-	{
+	for(i = 0, k = size - 1; i < k; i++, k--) {
 		p[i] ^= p[k];
 		p[k] ^= p[i];
 		p[i] ^= p[k];
@@ -85,6 +79,7 @@ int sprintf(char *out, const char *fmt, ...) {
   }
   va_end(ap);
 
+  *s = '\0';
   return arg_cnt;
 }
 
