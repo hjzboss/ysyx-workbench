a55fc499d726e61340fdb421ba60dae22692ce74
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-26 00:55:52 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:55:52 up 2 days, 19:44,  1 user,  load average: 0.14, 0.08, 0.02

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -52,7 +52,7 @@ void one_cycle(VJzCore* dut, VerilatedVcdC* tfp) {
   main_time++;
 
   if (main_time == 3) dut->reset = 0;
-  dut->clock = 0
+  dut->clock = 0;
   dut->io_inst = pmem_read(dut->io_pc);
   dut->eval();
   tfp->dump(main_time);
