ae945ab780c8ae84d58245707eee0f7592a3b969
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 21:39:20 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:39:20 up 3 days, 11:25,  1 user,  load average: 0.31, 0.12, 0.03

--- npc/csrc/monitor/sdb/expr.cpp
+++ npc/csrc/monitor/sdb/expr.cpp
@@ -104,7 +104,7 @@ static bool make_token(char *e) {
         int substr_len = pmatch.rm_eo;
 
         //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        //    i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
