f04fd094e0aedf79b3635293315f6c068e0158f2
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-28 21:45:46 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:45:46 up 3 days, 11:31,  1 user,  load average: 0.26, 0.12, 0.04

--- npc/csrc/monitor/sdb/sdb.cpp
+++ npc/csrc/monitor/sdb/sdb.cpp
@@ -16,8 +16,8 @@
 #include <cpu/cpu.h>
 #include <cpu/reg.h>
 #include <monitor/sdb.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+#include <readline.h>
+#include <history.h>
 // my change
 #include <memory/paddr.h>
 #include <macro.h>
