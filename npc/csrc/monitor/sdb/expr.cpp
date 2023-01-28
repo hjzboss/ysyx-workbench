6cd5998696d4ea1465225b2c4f298ea9769cd42c
tracer-ysyx2204 <tracer@ysyx.org>
2023-01-29 01:07:08 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:07:08 up 3 days, 14:53,  1 user,  load average: 0.10, 0.09, 0.04

--- npc/csrc/monitor/sdb/expr.cpp
+++ npc/csrc/monitor/sdb/expr.cpp
@@ -76,7 +76,7 @@ void init_regex() {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
-      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+      printf("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
 
