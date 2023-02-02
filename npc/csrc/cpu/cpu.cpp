c0a6706d514122959c8a8eece5769760e95c0d6d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 22:30:44 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:30:44 up 3 days, 23:27,  1 user,  load average: 0.12, 0.07, 0.03

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -185,8 +185,8 @@ static void statistic() {
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   printf("host time spent = " NUMBERIC_FMT " us", g_timer);
   printf("\ntotal guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
-  if (g_timer > 0) printf("\nsimulation frequency = " NUMBERIC_FMT " inst/s\n", g_nr_guest_inst * 1000000 / g_timer);
-  else printf("Finish running in less than 1 us and can not calculate the simulation frequency\n");
+  if (g_timer > 0) printf("\nsimulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
+  else printf("\nFinish running in less than 1 us and can not calculate the simulation frequency\n");
 }
 
 /* Simulate how the CPU works. */
