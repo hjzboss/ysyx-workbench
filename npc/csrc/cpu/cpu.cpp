e1429ea2d71537592f25ed7a9aee94ed7d507c8b
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-02 21:53:19 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:53:19 up 3 days, 22:50,  1 user,  load average: 0.17, 0.10, 0.08

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -212,6 +212,7 @@ void cpu_exec(uint64_t n) {
            (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           npc_state.halt_pc);
+          printf("\n");
       // fall through
     case NPC_QUIT: statistic();
   }
