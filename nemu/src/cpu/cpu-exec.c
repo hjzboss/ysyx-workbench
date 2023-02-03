3654bb86c494703deed53fe44f08a7951749f415
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-03 12:40:53 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:40:53 up 4 days,  2:16,  1 user,  load average: 0.28, 0.13, 0.04

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -44,7 +44,7 @@ static void insert_iringbuf(char* logbuf) {
   iring_ptr = (iring_ptr + 1) % MAX_INST_TO_PRINT;
   memcpy(iringbuf[iring_ptr], logbuf, strlen(logbuf));
   //iringbuf[iring_ptr] = logbuf;
-  puts(iringbuf[iring_ptr]);
+  //puts(iringbuf[iring_ptr]);
 }
 
 static void print_iringbuf() {
