e161489c9e89fe56ca06a0bf917e75a3d99ac214
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-03 12:08:06 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:08:06 up 4 days,  1:43,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -81,7 +81,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   printf("test: ");
-  puts(p);
+  puts(s->logbuf);
   insert_iringbuf(p);
 #endif
 }
