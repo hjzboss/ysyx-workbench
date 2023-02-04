513cecb47c17897b4b323a2a38dfefe38566668f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-04 21:46:53 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:53 up 5 days,  1:45,  1 user,  load average: 0.33, 0.14, 0.06

--- npc/csrc/monitor/monitor.cpp
+++ npc/csrc/monitor/monitor.cpp
@@ -27,13 +27,11 @@ void init_disasm(const char *triple);
 IFDEF(CONFIG_ITRACE, void init_iringbuf());
 
 static void welcome() {
-  /*
   Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
   IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
         "to record the trace. This may lead to a large log file. "
         "If it is not necessary, you can disable it in menuconfig"));
   //Log("Build time: %s, %s", __TIME__, __DATE__);
-  */
   printf("Welcome to %s-npc!\n", ANSI_FMT(str(riscv64), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
 }
