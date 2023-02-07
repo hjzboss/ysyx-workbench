03b71d063047f383d19378958c52109fb8259caf
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-07 22:48:41 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:48:41 up 5 days, 15:14,  1 user,  load average: 0.04, 0.07, 0.07

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -69,6 +69,7 @@ static void trace_and_difftest(uint64_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(cpu.logbuf)); }
   //printf("pc=%016lx\n", cpu.pc);
   //IFDEF(CONFIG_DIFFTEST, difftest_step());
+  cpu.gpr = gpr;
   printf("%lx\n", cpu.gpr[1]);
 	// watchpoint
 	//IFDEF(CONFIG_WATCHPOINT, scan_watchpoint(_this));
@@ -131,7 +132,6 @@ long init_cpu(char *dir) {
   cpu.pc = top->io_pc;
   cpu.npc = top->io_nextPc;
   cpu.gpr = gpr;
-  printf("%lx\n", cpu.gpr[1]);
 
   // state is running
   npc_state.state = NPC_RUNNING;
