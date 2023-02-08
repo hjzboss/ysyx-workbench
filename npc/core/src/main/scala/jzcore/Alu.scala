86d5a1dcb0e0d59f93d8e83c23040888f9e1ec8d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-09 00:31:01 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:31:01 up 5 days, 18:54,  1 user,  load average: 0.00, 0.01, 0.03

--- npc/core/src/main/scala/jzcore/Alu.scala
+++ npc/core/src/main/scala/jzcore/Alu.scala
@@ -22,22 +22,26 @@ class Alu extends Module {
     AluOp.add       -> (opA + opB),
     AluOp.jump      -> (opA + opB),
     AluOp.sub       -> (opA - opB),
-    AluOp.notEq     -> (opA - opB),
+    AluOp.beq       -> Mux(opA === opB, 1.U(64.W), 0.U(64.W)),
+    AluOp.bne       -> Mux(opA =/= opB, 1.U(64.W), 0.U(64.W)),
     AluOp.and       -> (opA & opB),
     AluOp.or        -> (opA | opB),
     AluOp.xor       -> (opA ^ opB),
     AluOp.slt       -> Mux(opA.asSInt() < opB.asSInt(), 1.U(64.W), 0.U(64.W)),
+    AluOp.blt       -> Mux(opA.asSInt() < opB.asSInt(), 1.U(64.W), 0.U(64.W)),
+    AluOp.bge       -> Mux(opA.asSInt() >= opB.asSInt(), 1.U(64.W), 0.U(64.W)),
+    AluOp.bgeu      -> Mux(opA >= opB, 1.U(64.W), 0.U(64.W)),
     AluOp.sltu      -> Mux(opA < opB, 1.U(64.W), 0.U(64.W)),
+    AluOp.bltu      -> Mux(opA < opB, 1.U(64.W), 0.U(64.W)),
     AluOp.sll       -> (opA << opB(5, 0)),
     AluOp.srl       -> (opA >> opB(5, 0)),
     AluOp.sra       -> (opA.asSInt() >> opB).asUInt(),
     // todo
     AluOp.div       -> (opA / opB),
-    AluOp.times     -> (opA * opB)
+    AluOp.mul       -> (opA * opB),
   ))
 
-  val zero = Mux(aluOut === 0.U(64.W), true.B, false.B)
-
+  val isOne = Mux(aluOp === 1.U(64.W), true.B, false.B)
   io.aluOut := aluOut
-  io.brMark := Mux(aluOp === AluOp.jump, true.B, Mux(aluOp === AluOp.notEq || aluOp === AluOp.slt || aluOp === AluOp.sltu, ~zero, zero))
+  io.brMark := Mux(aluOp === AluOp.jump, true.B, Mux(aluOp === AluOp.beq || aluOp === AluOp.bne || aluOp === AluOp.blt || aluOp === AluOp.bltu || aluOp === AluOp.bge || aluOp === AluOp.bgeu, isOne, false))
 }
