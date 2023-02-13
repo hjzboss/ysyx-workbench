64b803b3f2194b886c1294f5b545f56775fd8ec7
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 15:29:27 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:29:27 up 6 days, 15:17,  1 user,  load average: 0.23, 0.17, 0.11

--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -7,7 +7,7 @@ uint8_t* guest_to_host(uint64_t paddr);
 /* convert the host virtual address in NEMU to guest physical address in the guest program */
 uint64_t host_to_guest(uint8_t *haddr);
 
-uint64_t pmem_read(uint64_t addr, int len);
+uint64_t paddr_read(uint64_t addr, int len);
 //void paddr_write(paddr_t addr, int len, word_t data);
 
 #endif
