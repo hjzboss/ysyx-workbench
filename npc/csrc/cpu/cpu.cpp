1c12d4998741cbc484d411f6575fb27f1e5fb14c
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 19:13:28 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:13:28 up 6 days, 19:01,  1 user,  load average: 0.08, 0.09, 0.06

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -82,6 +82,7 @@ extern "C" void c_break() {
 
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
+  printf("debug: raddr=%lx\n", raddr);
   if(raddr < 0x80000000ull) return;
   printf("debug: raddr=%lx\n", raddr);
   *rdata = paddr_read(raddr & ~0x7ull, 8);
