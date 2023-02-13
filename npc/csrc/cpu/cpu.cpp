a01c222c4a4f8538d138e8d7924ca199c4e8552d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 21:44:11 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:44:11 up 6 days, 21:32,  1 user,  load average: 0.09, 0.04, 0.06

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -82,7 +82,6 @@ extern "C" void c_break() {
 
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
-  printf("raddr=%llx\n", raddr);
   if(raddr < 0x80000000ull) {
     *rdata = 0x00000013;
     return;
@@ -95,7 +94,6 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  printf("waddr=%llx\n", waddr);
   if(wmask == 0x00 || waddr < 0x80000000ull) return;
   int len;
   switch(wmask) {
