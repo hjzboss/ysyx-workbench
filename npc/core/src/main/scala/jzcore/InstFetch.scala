d68ca9d7556af4d37faba1fbaf2a04ba456926e8
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 20:06:56 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:56 up 6 days, 19:55,  1 user,  load average: 0.37, 0.24, 0.19

--- npc/core/src/main/scala/jzcore/InstFetch.scala
+++ npc/core/src/main/scala/jzcore/InstFetch.scala
@@ -7,6 +7,6 @@ import chisel3.util._
 class InstFetch extends BlackBox {
   val io = IO(new Bundle {
     val pc      = Input(UInt(64.W))
-    val inst    = Output(UInt(64.W))
+    val inst    = Output(UInt(32.W))
   })
 }
