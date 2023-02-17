6849cb190afa04be14cd4845fb19c33fb5c74ecf
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-17 21:35:22 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:35:22 up 23:19,  1 user,  load average: 0.50, 0.31, 0.20

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -85,8 +85,8 @@ static uint32_t *i8042_data_port_base = NULL;
 static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
-  printf("fuck!\n");
   i8042_data_port_base[0] = key_dequeue();
+  printf("%u\n", i8042_data_port_base[0]);
 }
 
 void init_i8042() {
