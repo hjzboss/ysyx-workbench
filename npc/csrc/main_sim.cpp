c2fab7213e4cfb68d2799fe51fb9b81ef3951fe6
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 11:48:31 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:48:31 up 1 day,  4:37,  1 user,  load average: 0.22, 0.09, 0.08

--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -6,6 +6,8 @@ void delete_cpu();
 
 int main(int argc, char** argv, char** env) {
 
+  printf("debug\n\n\n\n\n\n\n\n");
+
   // Prevent unused variable warnings
   if (0 && argc && argv && env) {}
   // Pass arguments so Verilated code can see them, e.g. $value$plusargs
@@ -16,7 +18,6 @@ int main(int argc, char** argv, char** env) {
 
   init_monitor(argc, argv);
 
-  printf("debug\n\n\n\n\n\n\n\n");
   // Simulate until $finish
   sdb_mainloop();
 
