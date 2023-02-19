0000f87567800c0f518537b7cecd0889b75975ef
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 23:12:53 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:12:53 up 1 day, 16:01,  1 user,  load average: 0.07, 0.14, 0.10

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -28,6 +28,7 @@ static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   }
 }
 
+/*
 #ifndef CONFIG_TARGET_AM
 static void timer_intr() {
   if (nemu_state.state == NEMU_RUNNING) {
@@ -36,6 +37,7 @@ static void timer_intr() {
   }
 }
 #endif
+*/
 
 void init_timer() {
   rtc_port_base = (uint32_t *)new_space(8);
@@ -44,5 +46,5 @@ void init_timer() {
 #else
   add_mmio_map("rtc", CONFIG_RTC_MMIO, rtc_port_base, 8, rtc_io_handler);
 #endif
-  IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
+  //IFNDEF(CONFIG_TARGET_AM, add_alarm_handle(timer_intr));
 }
