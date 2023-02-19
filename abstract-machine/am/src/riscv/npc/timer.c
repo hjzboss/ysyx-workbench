51943edb4236e1c8d034d03df72383b1b52d2707
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 11:48:12 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:48:12 up 1 day,  4:37,  1 user,  load average: 0.11, 0.06, 0.07

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -2,16 +2,13 @@
 #include <klib.h>
 
 void __am_timer_init() {
-  printf("shit!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("shit!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  printf("shit!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   rtc->second = 0;
   rtc->minute = 0;
   rtc->hour   = 0;
--- npc/csrc/main_sim.cpp
+++ npc/csrc/main_sim.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv, char** env) {
 
   init_monitor(argc, argv);
 
+  printf("debug\n\n\n\n\n\n\n\n");
   // Simulate until $finish
   sdb_mainloop();
 
