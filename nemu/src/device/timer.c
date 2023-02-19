9b419d4e09d037f4e64fd39321349b5a7f4d001f
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 23:09:40 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:09:40 up 1 day, 15:58,  1 user,  load average: 0.16, 0.12, 0.10

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -17,16 +17,14 @@
 #include <device/alarm.h>
 #include <utils.h>
 
-//static uint32_t *rtc_port_base = NULL;
-static uint64_t *rtc_port_base = NULL;
+static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  if (!is_write && offset == 0) {
+  if (!is_write && offset == 4) {
     uint64_t us = get_time();
-    //rtc_port_base[0] = (uint32_t)us;
-    //rtc_port_base[1] = us >> 32;
-    rtc_port_base[0] = us;
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = us >> 32;
   }
 }
 
@@ -40,7 +38,7 @@ static void timer_intr() {
 #endif
 
 void init_timer() {
-  rtc_port_base = (uint64_t *)new_space(8);
+  rtc_port_base = (uint32_t *)new_space(8);
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
