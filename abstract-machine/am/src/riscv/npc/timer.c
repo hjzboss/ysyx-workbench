890f520f02739f05e551ce6933544ae2f0a6635d
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-19 20:01:09 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:01:08 up 1 day, 12:50,  1 user,  load average: 0.27, 0.14, 0.04

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -11,7 +11,7 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   // todo
   uptime->us = (uint64_t)inl(RTC_ADDR);
-  printf("%lx\n", uptime->us);
+  printf("%d\n", uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
