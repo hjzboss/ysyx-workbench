2d6087eb6055090a3fca3b41920f407ddee170e6
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 14:45:48 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:45:48 up 3 days, 13:56,  1 user,  load average: 0.09, 0.09, 0.04

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -24,23 +24,26 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B, TYPE_R,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J, TYPE_B, TYPE_R, TYPE_Z,
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
+#define csrR() do { if (srcCsr == 0x305) *csr = 1; else if (srcCsr == 0x341) *csr = 2; else if (srcCsr == 0x300) *csr = 3; } while (0);
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1; } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1; } while(0)
+#define immZ() do { *imm = (uint64_t)BITS(i, 19, 15); } while(0)
 
-static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
+static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, word_t *csr, int type) {
   uint32_t i = s->isa.inst.val;
   int rd  = BITS(i, 11, 7);
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
+  int srcCsr = BITS(i, 31, 20);
   *dest = rd;
   switch (type) {
     case TYPE_I: src1R();          immI(); break;
@@ -49,17 +52,19 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_J: src1R();          immJ(); break;
     case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_R: src1R(); src2R();         break;
+    case TYPE_Z: src1R(); csrR();          break;
   }
 }
 
 static int decode_exec(Decode *s) {
   int dest = 0;
   word_t src1 = 0, src2 = 0, imm = 0;
+  word_t csr = 0;
   s->dnpc = s->snpc;
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
-  decode_operand(s, &dest, &src1, &src2, &imm, concat(TYPE_, type)); \
+  decode_operand(s, &dest, &src1, &src2, &imm, &csr, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
 
@@ -134,6 +139,13 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 101 ????? 11000 11", bge    , B, s->dnpc = (sword_t)src1 >= (sword_t)src2 ? s->pc + imm : s->snpc);
   INSTPAT("??????? ????? ????? 110 ????? 11000 11", bltu   , B, s->dnpc = src1 < src2 ? s->pc + imm : s->snpc);
   INSTPAT("??????? ????? ????? 111 ????? 11000 11", bgeu   , B, s->dnpc = src1 >= src2 ? s->pc + imm : s->snpc);
+
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] = src1);
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] |= src1);
+  INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] = cpu.csr[csr] & ~src1);
+  INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrwi , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] = imm);
+  INSTPAT("??????? ????? ????? 110 ????? 11100 11", csrrsi , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] |= imm);
+  INSTPAT("??????? ????? ????? 111 ????? 11100 11", csrrci , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] = cpu.csr[csr] & ~imm);
 
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(0, s->pc)); // R(10) is $a0
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
