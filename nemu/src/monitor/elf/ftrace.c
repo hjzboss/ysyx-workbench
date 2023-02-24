e2b8d0664b0487a87ae469af3a204f5c344becf1
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-24 22:40:32 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:40:32 up 3 days, 21:50,  1 user,  load average: 0.18, 0.08, 0.03

--- nemu/src/monitor/elf/ftrace.c
+++ nemu/src/monitor/elf/ftrace.c
@@ -175,6 +175,7 @@ void init_elf(const char *file) {
       func_list[func_num].start_addr = sym->st_value;
       func_list[func_num].size = sym->st_size;
       strcpy(func_list[func_num].name, name);
+      printf("%s\n", name);
       func_num++;
     }
     sym++;
