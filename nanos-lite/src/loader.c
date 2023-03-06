88506cd73361befbe4f1c1eb7da5d7e05365338d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 20:52:33 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:52:33 up 6 days,  1:58,  1 user,  load average: 0.18, 0.10, 0.04

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,9 +26,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   for (int i = 0; i < elf_head.e_phnum; i++, p_head++) {
     if (p_head->p_type != PT_LOAD) break;
     uint8_t *buf = malloc(p_head->p_filesz);
-    Log("%016x", *(uint64_t *)buf);
     ramdisk_read(buf, p_head->p_offset, p_head->p_filesz);
-
+    Log("%016x", *(uint64_t *)buf);
     memcpy((uint8_t *)p_head->p_vaddr, buf, p_head->p_filesz);
     memset((uint8_t *)(p_head->p_vaddr + p_head->p_filesz), 0, p_head->p_memsz - p_head->p_filesz);
     free(buf);
