aba6d4733dfee50a6daeb71f598b49240adef7c7
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-06 21:28:47 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:47 up 6 days,  2:35,  1 user,  load average: 0.16, 0.21, 0.15

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -33,7 +33,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     free(buf);
   }
   free(p_head);
-  return 0x83000000;
+  return elf_head.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
