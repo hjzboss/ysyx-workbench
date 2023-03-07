c647c00e0bbd5b54de3651150638037a459a587f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-07 12:13:24 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:13:24 up 6 days,  7:44,  1 user,  load average: 0.04, 0.05, 0.05

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -16,9 +16,8 @@ void do_syscall(Context *c) {
 
   switch (a[0]) {
     case SYS_yield: insert_strace("SYS_yield", a, c->GPRx); yield(); break;
-    //case SYS_exit: insert_strace("SYS_exit", a, c->GPRx); print_strace(); free_strace(); halt(0); break;
+    case SYS_exit: insert_strace("SYS_exit", a, c->GPRx); print_strace(); free_strace(); halt(0); break;
     //case SYS_write: break;
-    case SYS_exit: break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
