a08336ead6c41965708bf74919571d15e44cf11a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-08 12:17:41 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:17:41 up 6 days,  8:58,  1 user,  load average: 0.04, 0.11, 0.08

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 int main() {
-  write(1, "Hello World!\n", 13);
+  if(write(1, "Hello World!\n", 13) != 13) {
+    return 0;
+  } 
   int i = 2;
   volatile int j = 0;
   while (1) {
