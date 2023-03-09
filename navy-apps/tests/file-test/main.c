8c9624b781a0ccb0b5d975a2bcb8ce1ed760c581
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-09 20:37:00 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:37:00 up 7 days,  8:10,  1 user,  load average: 0.18, 0.11, 0.08

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -11,29 +11,30 @@ int main() {
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
+  printf("shit\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("shit1\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
-
+  printf("shit2\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-
+  printf("shit3\n");
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
-
+  printf("shit4\n");
   fclose(fp);
-
+  printf("shit5\n");
   printf("PASS!!!\n");
 
   return 0;
