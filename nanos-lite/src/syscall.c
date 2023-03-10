ef9f70c947580c11ace3611dd38bfb910a47024d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 14:57:13 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:57:13 up 7 days, 16:24,  1 user,  load average: 0.23, 0.18, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -100,8 +100,8 @@ void syscall_close(Context *c, uintptr_t *a) {
 void syscall_gettimeofday(Context *c, uintptr_t *a) {
   // 此处返回的是系统启动的时间，todo
   uint64_t us = io_read(AM_TIMER_UPTIME).us;
-  ((struct timeval *)a[1])->tv_usec = us;
-  ((struct timeval *)a[1])->tv_sec = us / 1000000;
+  (*(struct timeval **)a[1])->tv_usec = us;
+  (*(struct timeval **)a[1])->tv_sec = us / 1000000;
   // todo: timezone
   c->GPRx = 0;
 }
