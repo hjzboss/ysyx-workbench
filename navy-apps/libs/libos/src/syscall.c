b81e94aa154b838555909e77241e0d4f695b22f4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 15:17:45 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:17:45 up 7 days, 16:45,  1 user,  load average: 0.07, 0.03, 0.05

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -94,7 +94,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
-  return _syscall_(SYS_gettimeofday, (__uint64_t)&tv, (__uint64_t)&tz, 0);
+  return _syscall_(SYS_gettimeofday, (__uint64_t)tv, (__uint64_t)tz, 0);
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
