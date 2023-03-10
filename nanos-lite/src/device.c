b2e3623d83d1c7668dbffb7f510c5d8feca1cd40
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 22:09:26 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:26 up 7 days, 23:37,  1 user,  load average: 0.25, 0.18, 0.06

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,7 +29,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   if (ev.keycode == AM_KEY_NONE) {
     return 0;
   }
-  sprintf(buf, "Got  (kbd): %s (%d) %s\n", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
+  sprintf(buf, "Got  (kbd): %s (%d) %s", keyname[ev.keycode], ev.keycode, ev.keydown ? "DOWN" : "UP");
   return strlen(buf);
 }
 
