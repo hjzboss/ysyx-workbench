e7d91551398ae8ee3690b1157ac34aaef1b6e74f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 15:41:38 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:41:38 up 7 days, 17:09,  1 user,  load average: 0.61, 0.20, 0.11

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -4,15 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+uint32_t NDL_GetTicks() {
   struct timeval *tv = (struct timeval *)malloc(sizeof(struct timeval));
-  uint64_t old_usec = 0;
-  while (gettimeofday(tv, NULL) == 0) {
-    uint64_t usec = tv->tv_usec;
-    //printf("%lu\n", usec);
-    if (usec - old_usec >= 500000) {
+  gettimeofday(tv, NULL);
+  return tv->tv_usec / 1000;
+}
+
+int main() {
+  uint32_t old_ms = 0;
+  while (1) {
+    uint32_t ms = NDL_GetTicks();
+    if (ms - old_ms >= 500) {
       printf("test\n");
-      old_usec = usec;
+      old_ms = ms;
     }
   }
   return 0;
