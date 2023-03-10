84e90b862a011086573f4fb9f5db9f843c23dd54
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-10 15:25:56 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:25:56 up 7 days, 16:53,  1 user,  load average: 0.37, 0.17, 0.10

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -9,8 +9,8 @@ int main() {
   uint64_t old_usec = 0;
   while (gettimeofday(tv, NULL) == 0) {
     uint64_t usec = tv->tv_usec;
-    printf("%lu\n", usec);
-    if (usec - old_usec == 500000) {
+    //printf("%lu\n", usec);
+    if (usec - old_usec >= 500000) {
       printf("test\n");
       old_usec = usec;
     }
