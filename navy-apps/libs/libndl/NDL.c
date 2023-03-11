dc814d9aaf24906b5f5370f133ae31a9d0666f97
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 23:59:30 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:59:30 up 8 days, 14:26,  1 user,  load average: 1.16, 0.41, 0.20

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -74,6 +74,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  printf("NDL: x=%d, y=%d\n", x, y);
   int fb_fd = _open("/dev/fb", 0, 0);
   lseek(fb_fd, (y * screen_w + x) * 4, SEEK_SET);
   write(fb_fd, pixels, w * h * 4);
