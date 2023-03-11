fcd663def9c808df428552cd0a37a8c198c3799f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 22:29:45 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:29:45 up 8 days, 12:56,  1 user,  load average: 0.25, 0.21, 0.11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -100,17 +100,19 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   // todo
+  /*
   if (file_table[fd].write != NULL) {
     file_table[fd].write(buf, 0, len);
     return len;
   }
+  */
   size_t size = file_table[fd].size;
   size_t open_offset = file_table[fd].open_offset;
   if (fd != FD_STDOUT && fd != FD_STDERR)
     assert(len + open_offset <= size);
   size_t offset = file_table[fd].disk_offset + open_offset;
   size_t upper_bound = file_table[fd].disk_offset + size;
-  size_t rem = offset + len > upper_bound ? (upper_bound - offset) : len;
+  size_t rem = offset + len >= upper_bound ? (upper_bound - offset) : len;
   if (file_table[fd].write != NULL) {
     file_table[fd].write(buf, offset, rem);
   }
