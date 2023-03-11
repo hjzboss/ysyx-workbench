1fb02d31c8aa89a7b048a91a9648f434be18cf9a
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-11 15:46:47 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:46:47 up 8 days,  6:13,  1 user,  load average: 0.22, 0.16, 0.08

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -83,7 +83,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
   size_t upper_bound = file_table[fd].disk_offset + size;
   size_t rem = offset + len > upper_bound ? (upper_bound - offset) : len;
   if (file_table[fd].read != NULL) {
-    file_table[fd].read(buf, offset, rem);
+    return file_table[fd].read(buf, offset, rem);
   }
   else {
     ramdisk_read(buf, offset, rem);
