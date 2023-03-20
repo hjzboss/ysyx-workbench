e57c0f2228fae622faa701c5b71200fa87e14b9f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-20 11:39:07 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:39:07 up 10 days, 10:30,  1 user,  load average: 0.35, 0.15, 0.04

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -26,6 +26,7 @@ void render() {
   char fname[256];
   sprintf(fname, path, cur);
   slide = SDL_LoadBMP(fname);
+  printf("%x\n", slide->pixels[0]);
   assert(slide);
   SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
@@ -50,11 +51,8 @@ int main() {
 
   int rep = 0, g = 0;
 
-  while (1) {
-    render();
-  }
-  //render();
-  
+  render();
+
   /*
   while (1) {
     SDL_Event e;
