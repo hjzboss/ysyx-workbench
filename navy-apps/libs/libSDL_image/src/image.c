3128403245f735274913dd0e3d5868e0ba8f88e5
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 22:04:33 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:04:33 up  6:35,  1 user,  load average: 0.23, 0.16, 0.10

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -17,7 +17,8 @@ SDL_Surface* IMG_Load(const char *filename) {
   size_t size;
   fseek(fp, 0, SEEK_END);
   size = ftell(fp); // 获取文件大小
-
+  
+  fseek(fp, 0, SEEK_SET);
   uint8_t *buf = (uint8_t *)malloc(size);
   size_t num = fread(buf, 1, size, fp);
   assert(num == size);
