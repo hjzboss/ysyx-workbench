5becc2ba483cba6661a75e1c984fe5faf12281a8
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-27 18:33:14 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:14 up 2 days, 16:26,  1 user,  load average: 0.18, 0.10, 0.04

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -147,7 +147,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? 110 ????? 11100 11", csrrsi , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] |= imm);
   INSTPAT("??????? ????? ????? 111 ????? 11100 11", csrrci , Z, R(dest) = cpu.csr[csr]; cpu.csr[csr] = cpu.csr[csr] & ~imm);
 
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, TODO();s->dnpc = isa_raise_intr(0, s->pc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(0, s->pc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = cpu.csr[2] + 4); // mepc + 4
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -19,6 +19,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  printf("shit\n");
   cpu.csr[3] = 0xb; // mcause syscall
   cpu.csr[2] = epc;  // mepc
   return cpu.csr[1]; // mtvec
