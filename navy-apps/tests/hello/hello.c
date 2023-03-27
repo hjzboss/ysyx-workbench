11167e9a6ab9ccfc6ef8388bb70673ee9d5b5fb6
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-27 21:03:49 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:03:49 up 2 days, 18:57,  1 user,  load average: 0.16, 0.16, 0.12

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,6 +5,7 @@ int main() {
   if(write(1, "Hello World!\n", 13) != 13) {
     return 0;
   }
+/*
   int i = 2;
   volatile int j = 0;
   while (1) {
@@ -14,5 +15,6 @@ int main() {
       j = 0;
     }
   }
+*/
   return 0;
 }
