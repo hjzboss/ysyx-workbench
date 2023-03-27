14c2e6185cf8bba43305b94fa4cf232edafd0c04
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-27 22:31:47 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:31:47 up 2 days, 20:25,  1 user,  load average: 0.09, 0.04, 0.01

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -11,7 +11,6 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t data = (uint32_t)inl(VGACTL_ADDR);
-  printf("data=%x\n", data);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = data >> 16, 
