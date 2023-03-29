4490ba3c114fd13a82bc7dd2b7d6e740e45f574e
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-29 10:45:36 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:45:36 up 12:19,  3 users,  load average: 0.76, 0.84, 0.79

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -56,6 +56,7 @@ static void init_screen() {
 }
 
 static inline void update_screen() {
+  printf("shit\n");
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
