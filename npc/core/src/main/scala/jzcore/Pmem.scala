f4b110b45b79f9baaf0f4ba7364e03d27513b910
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 23:36:25 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:36:25 up 5 days,  1:10,  3 users,  load average: 0.41, 0.72, 0.78

--- npc/core/src/main/scala/jzcore/Pmem.scala
+++ npc/core/src/main/scala/jzcore/Pmem.scala
@@ -7,7 +7,7 @@ import utils._
 class Pmem extends BlackBox {
   val io = IO(new Bundle {
     val raddr   = Input(UInt(64.W))
-    val rdata   = Output(UInt(32.W))
+    val rdata   = Output(UInt(64.W))
     val rvalid  = Input(Bool())
   })
 }
