7c9f39c5e01ecbf7406ffb571c68cc1d59aa7cd0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 00:41:51 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:41:51 up 6 days,  2:16,  3 users,  load average: 0.81, 0.55, 0.62

--- npc/core/src/main/scala/jzcore/Sram.scala
+++ npc/core/src/main/scala/jzcore/Sram.scala
@@ -10,11 +10,13 @@ class Sram extends Module {
     val rdataIO   = Decoupled(new RdataIO)
   })
 
+  val raddrFire          = io.raddrIO.valid && io.raddrIO.ready
+
   // read state
   val ar_wait :: fetch :: Nil = Enum(2)
   val rState = RegInit(ar_wait)
   rState := MuxLookup(rState, ar_wait, List(
-    ar_wait       -> Mux(io.raddrIO.valid && io.raddrIO.ready, fetch, ar_wait), // 等待地址信息
+    ar_wait       -> Mux(raddrFire, fetch, ar_wait), // 等待地址信息
     fetch         -> Mux(io.rdataIO.valid && io.rdataIO.ready, ar_wait, fetch), // 取指完成，当取指阻塞时保持状态
   ))
 
@@ -22,12 +24,10 @@ class Sram extends Module {
 
   io.raddrIO.ready      := true.B // todo
 
-  val raddrFire          = io.raddrIO.valid && io.raddrIO.ready
-
   io.rdataIO.valid      := rState === fetch
   io.rdataIO.bits.rresp := 0.U(2.W)
 
   pmem.io.raddr         := io.raddrIO.bits.addr
-  pmem.io.rvalid        := rState === fetch
+  pmem.io.rvalid        := rState === ar_wait && raddrFire
   io.rdataIO.bits.data  := pmem.io.rdata
 }
