ea057a5d859ecffe70f79185745213affb33a54a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 21:21:24 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:21:24 up  1:43,  1 user,  load average: 0.07, 0.11, 0.15

--- npc/core/src/main/verilog/RF.sv
+++ npc/core/src/main/verilog/RF.sv
@@ -24,7 +24,8 @@ assign src1 = rf[rs1];
 assign src2 = rf[rs2];
 
 always @(posedge clock) begin
-  $display("shit");
+  if (wen) 
+    $display("shit");
   if (wen && waddr != 5'd0) begin
     rf[waddr] <= wdata;
     $strobe("%h", rf[waddr]);
