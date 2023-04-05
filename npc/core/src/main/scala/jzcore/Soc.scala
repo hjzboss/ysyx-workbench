320cbea790db2bc8e232937e90a7c08f7a4bbf09
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 20:54:02 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:54:02 up 11:38,  1 user,  load average: 0.27, 0.56, 0.44

--- npc/core/src/main/scala/jzcore/Soc.scala
+++ npc/core/src/main/scala/jzcore/Soc.scala
@@ -16,6 +16,13 @@ class Soc extends Module {
   core.io.axiRaddrIO <> sram.io.raddrIO
   core.io.axiRdataIO <> sram.io.rdataIO
 
+  sram.io.waddrIO.valid := false.B
+  sram.io.waddrIO.bits.addr := "0x80000000".U
+  sram.io.wdataIO.valid := false.B
+  sram.io.wdataIO.bits.wdata := 0.U
+  sram.io.wdataIO.bits.wstrb := 0.u
+  sram.io.brespIO.ready := false.B
+
   // 仿真环境
   io.debug        := core.io.debug
 }
