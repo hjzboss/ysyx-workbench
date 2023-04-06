b339ca359781e4cb3cdf939ae3a7cc981c1ae152
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-07 01:23:56 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:23:56 up 15:54,  1 user,  load average: 0.81, 0.80, 0.72

--- npc/core/src/main/scala/jzcore/LSU.scala
+++ npc/core/src/main/scala/jzcore/LSU.scala
@@ -77,7 +77,7 @@ class LSU extends Module {
   val align              = addr(2, 0) << 3.U
   val alignReg           = RegInit(0.U(5.W))
   alignReg              := Mux(readTrans, align, alignReg)
-  io.align              := align
+  io.align              := alignReg
   val rdata              = io.rdataIO.bits.rdata >> alignReg
   val lsuOut             = LookupTree(lsTypeReg, Seq(
     LsType.ld   -> rdata,
