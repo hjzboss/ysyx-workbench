e158b334f6b7bf1be6e609ce6bdb29bf641f67d4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 09:40:51 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:40:51 up 11 min,  1 user,  load average: 0.56, 0.41, 0.20

--- npc/core/src/main/scala/jzcore/LSU.scala
+++ npc/core/src/main/scala/jzcore/LSU.scala
@@ -94,5 +94,5 @@ class LSU extends Module {
   io.out.csrWaddr       := io.in.csrWaddr
   io.out.csrWen         := Mux(io.lsuReady && !io.stall, io.in.csrWen, false.B)
 
-  io.lsuReady           := (!io.in.ren && !io.in.wen) || (((rState === wait_data && rdataFire) || (wState === wait_resp && brespFire)) && (rresp === okay || bresp === okay))
+  io.lsuReady           := (!io.in.ren && !io.in.wen) || (((rState === wait_data) || (wState === wait_resp)) && (rresp === okay || bresp === okay))
 }
