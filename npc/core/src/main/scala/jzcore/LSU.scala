dac5428e33fb5cd7bb92054aa089b97983144ce3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 23:19:20 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:19:20 up 13:49,  1 user,  load average: 1.84, 0.95, 0.63

--- npc/core/src/main/scala/jzcore/LSU.scala
+++ npc/core/src/main/scala/jzcore/LSU.scala
@@ -121,7 +121,8 @@ class LSU extends Module {
   io.out.csrWen         := Mux(!io.lsuReady || io.stall, false.B, Mux(rState === wait_data || wState === wait_resp, csrWenreg, io.in.csrWen))
 
   io.lsuTrans           := (rState === idle && raddrFire) || (wState === idle && waddrFire) || rState === wait_data || wState === wait_resp
-  io.lsuReady           := !hasTrans || (((rState === wait_data && rdataFire) || (wState === wait_resp && brespFire)) && (rresp === okay || bresp === okay))
+  //io.lsuReady           := !hasTrans || (((rState === wait_data && rdataFire) || (wState === wait_resp && brespFire)) && (rresp === okay || bresp === okay))
+  io.lsuReady           := true.B
   //val lsuReady           = RegInit(true.B)
   //lsuReady              := ((rState === wait_data && rdataFire) || (wState === wait_resp && brespFire)) && (rresp === okay || bresp === okay)
   //io.lsuReady           := Mux(!io.in.ren && !io.in.wen, true.B, lsuReady)
