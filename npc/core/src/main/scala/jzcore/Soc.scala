927640f0abf40245159557f1d4718ab72425704b
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 00:24:25 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:24:25 up 10 min,  1 user,  load average: 1.37, 1.31, 0.74

--- npc/core/src/main/scala/jzcore/Soc.scala
+++ npc/core/src/main/scala/jzcore/Soc.scala
@@ -14,26 +14,15 @@ class Soc extends Module {
     val valid2     = Output(Bool())
   })
 
-  val rsram = Module(new Sram)
-  val wsram = Module(new Sram)
+  //val rsram = Module(new Sram)
+  val sram = Module(new Sram)
   val core  = Module(new JzCore)
 
-  core.io.axiRaddrIO <> rsram.io.raddrIO
-  core.io.axiRdataIO <> rsram.io.rdataIO
-  // todo: 实现仲裁
-  rsram.io.waddrIO.valid := false.B
-  rsram.io.waddrIO.bits.addr := "h80000000".U
-  rsram.io.wdataIO.valid := false.B
-  rsram.io.wdataIO.bits.wdata := 0.U
-  rsram.io.wdataIO.bits.wstrb := 0.U
-  rsram.io.brespIO.ready := false.B
-
-
-  core.io.raddrIO    <> wsram.io.raddrIO
-  core.io.rdataIO    <> wsram.io.rdataIO
-  core.io.axiWaddrIO <> wsram.io.waddrIO
-  core.io.axiWdataIO <> wsram.io.wdataIO
-  core.io.axiBrespIO <> wsram.io.brespIO
+  core.io.axiRaddrIO <> sram.io.raddrIO
+  core.io.axiRdataIO <> sram.io.rdataIO
+  core.io.axiWaddrIO <> sram.io.waddrIO
+  core.io.axiWdataIO <> sram.io.wdataIO
+  core.io.axiBrespIO <> sram.io.brespIO
 
   // 仿真环境
   io.debug        := core.io.debug
