7cd673c1d6792f691dfa55a8031201656a70a779
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 00:35:39 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:35:39 up 21 min,  1 user,  load average: 1.60, 1.68, 1.26

--- npc/core/src/main/scala/jzcore/CORE/JzCore.scala
+++ npc/core/src/main/scala/jzcore/CORE/JzCore.scala
@@ -40,7 +40,6 @@ class JzCore extends Module {
     is (0.U) {
       // 没有请求
       io.axiRaddrIO.valid       := false.B
-      io.axiRdataIO.bits.rdata    := 0.U
       io.axiRdataIO.ready       := false.B
       io.axiWaddrIO.valid       := false.B
       io.axiWdataIO.bits.wdata  := 0.U
