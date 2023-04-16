fcd65bceebb56745b65eafea0f63bf4f92f6c26f
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 21:59:46 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:59:46 up  1:57,  1 user,  load average: 0.29, 0.29, 0.28

--- npc/csrc/cpu/cpu.cpp
+++ npc/csrc/cpu/cpu.cpp
@@ -311,7 +311,7 @@ static void cpu_exec_once() {
 #endif
   //npc_cpu.pc = top->io_debug_pc; // 执行后的pc
   npc_cpu.pc = top->io_debug_nextPc; // next pc
-  printf("shit: %016x\n", npc_cpu.pc);
+  printf("shit: %016x\n", top->io_debug_nextPc);
   //npc_cpu.npc = top->io_debug_nextPc;
 #ifdef CONFIG_ITRACE
   char *p = npc_cpu.logbuf;
