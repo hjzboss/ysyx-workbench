949b3961801e5c5a5233301320dee9c046e655b4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 08:33:40 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:33:40 up  3:55,  1 user,  load average: 0.01, 0.14, 0.24

--- npc/core/src/main/scala/jzcore/CORE/ID_REG.scala
+++ npc/core/src/main/scala/jzcore/CORE/ID_REG.scala
@@ -27,8 +27,8 @@ class ID_REG extends Module with HasResetVector {
   regReset.inst := Instruction.NOP
 
   // 流水线寄存器
-  val idReg = RegInit(regReset)
-  idReg := Mux(io.flush, regReset, Mux(io.stall, idReg, io.in))
+  val idReg = RegEnable(io.in, regReset, !io.stall)
+  //idReg := Mux(io.flush, regReset, Mux(io.stall, idReg, io.in))
 
   val validReg = RegInit(false.B)
   validReg := Mux(io.flush, false.B, Mux(io.stall, validReg, io.validIn))
