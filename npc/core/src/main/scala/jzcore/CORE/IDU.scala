204ab95bd7d4e56103fa64c06f6ad1d1f39741a9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 23:55:50 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:55:50 up 14:12,  1 user,  load average: 0.46, 0.48, 0.48

--- npc/core/src/main/scala/jzcore/CORE/IDU.scala
+++ npc/core/src/main/scala/jzcore/CORE/IDU.scala
@@ -110,8 +110,8 @@ class IDU extends Module with HasInstrType{
   io.ctrl.memRen      := memEn === MemEn.load
   io.ctrl.ebreak      := systemCtrl === System.ebreak
   io.ctrl.sysInsType  := systemCtrl
-  io.ctrl.rs1         := rs1
-  io.ctrl.rs2         := rs2
+  io.ctrl.rs1         := Mux(instrtype === InstrZ, 0.U(5.W), rs1)
+  io.ctrl.rs2         := Mux(instrtype === InstrZ, rs1, rs2)
 
   io.aluCtrl.aluSrc1  := aluSrc1
   io.aluCtrl.aluSrc2  := aluSrc2
