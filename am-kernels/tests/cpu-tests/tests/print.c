0e226926b21f4ddb22edf7949425ce60985fb765
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-21 23:29:32 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:29:32 up 13:22,  1 user,  load average: 0.76, 0.87, 0.82

--- am-kernels/tests/cpu-tests/tests/print.c
+++ am-kernels/tests/cpu-tests/tests/print.c
@@ -1,9 +1,10 @@
 #include "trap.h"
 
 int main() {
-  char *shit = "shit";
-  int b = 123;
-  void *a = &b;
-  printf("%s = %p\n", shit, a);
+  //char *shit = "shit";
+  //int b = 123;
+  //void *a = &b;
+  //printf("%s = %p\n", shit, a);
+  printf("fuck\n\n\n");
   return 0;
 }
