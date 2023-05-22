94922ca16713e816db8608ac77c1d1fd135cee70
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-22 12:06:48 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:06:48 up 1 day,  1:59,  1 user,  load average: 0.95, 0.80, 0.64

--- am-kernels/tests/cpu-tests/tests/print.c
+++ am-kernels/tests/cpu-tests/tests/print.c
@@ -5,8 +5,8 @@ int main() {
   //int b = 123;
   //void *a = &b;
   //printf("%s = %p\n", shit, a);
-  *(int *)0x8aa00000 = 12345;
-  int *shit = (int *)0x8aa00000;
+  *(int *)0x85a00000 = 12345;
+  int *shit = (int *)0x85a00000;
   printf("fuck=%d\n", *shit);
   return 0;
 }
