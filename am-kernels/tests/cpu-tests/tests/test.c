69baad4fc0a6f6391948da81491f1d3cf4131dd7
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 00:08:45 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:08:45 up 14:39,  1 user,  load average: 2.05, 1.13, 0.94

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -6,10 +6,10 @@ int fuck[65535] = {};
 int main() {
   int i;
   //char *ptr = (char *)0x81000000;
-  for(i = 0; i < 260; i++) {
+  for(i = 0; i < 265; i++) {
     fuck[i] = i;
   }
-  for(i = 0; i < 260; i++) {
+  for(i = 0; i < 265; i++) {
     check(fuck[i] == i);
   }
 
