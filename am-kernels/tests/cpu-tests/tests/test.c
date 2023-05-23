ca744e21415980d8e9174311debb0792f1d6c1bc
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-23 12:22:34 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:22:34 up  2:52,  1 user,  load average: 0.73, 0.73, 0.64

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -1,6 +1,16 @@
 #include "klib.h"
+#include "trap.h"
 
 int main() {
-  printf("shit\n");
+  char i;
+  char *ptr = (char *)0x81000000;
+  for(i = 0; i < 8; i++) {
+    *(char *)(ptr + i) = i;
+  }
+  for(i = 0; i < 8; i++) {
+    char a = *(char *)(ptr + i);
+    check(a == i);
+  }
+
   return 0;
 }
