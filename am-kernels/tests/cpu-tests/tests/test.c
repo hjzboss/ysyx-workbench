95dc4c2a7c35e392e2cac8cb25a45980db13967f
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-24 00:01:51 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:51 up 14:32,  1 user,  load average: 0.79, 0.81, 0.81

--- am-kernels/tests/cpu-tests/tests/test.c
+++ am-kernels/tests/cpu-tests/tests/test.c
@@ -1,15 +1,15 @@
 #include "klib.h"
 #include "trap.h"
 
-int fuck[65535] = {};
+int fuck[12345] = {};
 
 int main() {
   int i;
   //char *ptr = (char *)0x81000000;
-  for(i = 0; i < 65535; i++) {
+  for(i = 0; i < 12345; i++) {
     fuck[i] = i;
   }
-  for(i = 0; i < 65535; i++) {
+  for(i = 0; i < 12345; i++) {
     check(fuck[i] == i);
   }
 
