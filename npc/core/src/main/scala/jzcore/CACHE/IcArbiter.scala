5c589f53274e7d9e0551c1559228e91200b8a818
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-31 16:09:44 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:09:44 up 30 min,  1 user,  load average: 0.53, 0.52, 0.39

---
+++ npc/core/src/main/scala/jzcore/CACHE/IcArbiter.scala
@@ -0,0 +1,25 @@
+package jzcore
+
+import chisel3._
+import chisel3.util._
+import utils._
+
+
+class IcArbiter extends Module {
+  val io = IO(new Bundle {
+    val stage3addr = Input(UInt(6.W))
+    val stage1Addr = Input(UInt(6.W))
+    val stage3Cen  = Input(Bool())
+    val stage1Cen  = Input(Bool())
+    val stage3Wen  = Input(Bool())
+    val stage1Wen  = Input(Bool())
+
+    val arbAddr    = Output(UInt(6.W))
+    val arbCen     = Output(Bool())
+    val arbWen     = Output(Bool())
+  })
+
+  io.arbAddr     := Mux(io.stage3Cen, io.stage1Addr, io.stage3Addr)
+  io.arbCen      := Mux(io.stage3Cen, io.stage1Cen, io.stage3Cen)
+  io.arbWen      := Mux(io.stage3Wen, io.stage1Wen, io.stage3Wen)
+}
