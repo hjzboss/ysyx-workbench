19f0268b595cd44dc0a86690b624d52f12b66c0e
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-03 13:12:39 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:12:39 up  3:37,  1 user,  load average: 1.23, 1.06, 0.81

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -121,7 +121,10 @@ void print_dtrace();
 #endif
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+#ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
+#endif
+  log_write("%s\n", _this->logbuf);
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 	// watchpoint
