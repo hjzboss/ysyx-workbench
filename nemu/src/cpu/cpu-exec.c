77ee520b2d3f81514fddea310793fe8a9e1f7ba6
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-03 19:53:42 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:53:42 up 10:18,  1 user,  load average: 1.31, 0.82, 0.55

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -126,7 +126,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   log_write("%s\n", _this->logbuf);
   if (g_print_step) { 
-    printf("shit\n");
     IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); 
   }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
