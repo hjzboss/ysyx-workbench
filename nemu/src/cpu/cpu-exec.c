8a4b3853c7705689e7237479cbe3bc3c68a7b280
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-03 20:47:15 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:47:15 up 11:11,  1 user,  load average: 0.87, 0.86, 0.91

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -124,8 +124,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
-  printf("%s\n", _this->logbuf);
-  if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 	// watchpoint
@@ -141,6 +139,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   cpu.inst = s->isa.inst.val;
   //printf("nemu_inst=%08x\n", cpu.inst);
 #ifdef CONFIG_ITRACE
+  printf("shit\n");
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
