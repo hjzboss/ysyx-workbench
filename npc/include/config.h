b13f96bd0acadc6996c89679d202fecdeebb810c
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-10 12:33:49 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:33:49 up 6 min,  1 user,  load average: 1.70, 1.83, 0.94

--- npc/include/config.h
+++ npc/include/config.h
@@ -9,10 +9,10 @@
 
 #define RESET_ADDR 0x80000000
 #define MAX_SIM_TIME 1000 // max simulation time
-//#define CONFIG_TRACE 1
+#define CONFIG_TRACE 1
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_MSIZE 0x8000000
-//#define CONFIG_ITRACE 1
+#define CONFIG_ITRACE 1
 //#define CONFIG_FTRACE 1
 #define CONFIG_DIFFTEST 1
 //#define CONFIG_MTRACE 1
