008463014bd117b19d4ea1efcf28e57ab23c33b3
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-14 10:19:12 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:19:12 up 47 min,  1 user,  load average: 1.93, 1.52, 0.89

--- npc/core/test/src/JzCoreSpec.scala
+++ npc/core/test/src/JzCoreSpec.scala
@@ -62,9 +62,9 @@ class PGenerator extends Module {
     val yAdd  = Input(Bool())
     val y     = Input(Bool())
     val ySub  = Input(Bool())
-    val x     = Input(UInt(68.W))
+    val x     = Input(UInt(132.W))
 
-    val p     = Output(UInt(68.W))
+    val p     = Output(UInt(132.W))
     val c     = Output(Bool())
   })
   
@@ -75,10 +75,10 @@ class PGenerator extends Module {
   val selDoubleNegative = io.yAdd & ~io.y & ~io.ySub
   val selDoublePositive = ~io.yAdd & io.y & io.ySub
 
-  val p_tmp = VecInit(List.fill(68)(false.B))
+  val p_tmp = VecInit(List.fill(132)(false.B))
 
-  (0 to 67).map(i => (p_tmp(i) := ~(~(selNegative & ~x(i+1)) & ~(selDoubleNegative & ~x(i)) & ~(selPositive & x(i+1)) & ~(selDoublePositive & x(i)))))
-
+  (0 to 131).map(i => (p_tmp(i) := ~(~(selNegative & ~x(i+1)) & ~(selDoubleNegative & ~x(i)) & ~(selPositive & x(i+1)) & ~(selDoublePositive & x(i)))))
+  //printf("io.p=%x\n", io.p)
   io.p := p_tmp.asUInt()
   io.c := selNegative | selDoubleNegative
 } 
@@ -111,8 +111,8 @@ class BoothTest2 extends Module {
 
   when(state === idle && io.valid) {
     result := 0.U
-    multiplicand := SignExt(io.a, 132) // todo
-    multiplier := io.b(63) ## io.a ## false.B
+    multiplicand := io.a // todo
+    multiplier := io.b(63) ## io.b ## false.B
   }.elsewhen(state === busy && !io.ready) {
     result := pg.io.p + result + pg.io.c
     multiplicand := multiplicand << 2.U
@@ -123,8 +123,14 @@ class BoothTest2 extends Module {
     multiplier := multiplier
   }
 
-  io.ready  := !multiplier.orR
-  io.c      := result
+  printf("result=%x, multiplier=%x, p=%x, c=%d\n", result, multiplier, pg.io.p, pg.io.c)
+  io.ready  := !multiplier.orR && state === busy
+  io.c      := result(127, 0)
+  when(io.ready) {
+    val ref = io.a * io.b
+    chisel3.assert(io.c === ref)
+    printf("io.c=%x, ref=%x\n", io.c, ref)
+  }
 }
 /*
 object JzCoreSpec extends TestSuite {
@@ -142,17 +148,17 @@ object JzCoreSpec extends ChiselUtestTester {
     test("mul") {
       testCircuit(new BoothTest2) {
         dut =>
-          dut.io.a.poke(894.U(64.W))
-          dut.io.b.poke(99.U(64.W))
+          dut.io.a.poke("h12311231234fffff".U(64.W))
+          dut.io.b.poke("hfffffffffffffff".U(64.W))
           var flag = true
           dut.io.valid.poke(true.B)
           while(flag) {
             dut.clock.step()
-            when(dut.io.ready.peek() === true.B) {
+            if(dut.io.ready.peek.litToBoolean) {
               flag = false
             }
           }
-          dut.io.c.expect((894 * 99).U(128.W))
+          dut.clock.step()
       }
     }
   }
