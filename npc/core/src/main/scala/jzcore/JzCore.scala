7e91c3e20a7550146ea63d8319a781e9f85d3565
tracer-ysyx2204 <tracer@ysyx.org>
2023-07-21 23:58:00 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-Victus-by-HP-15L-Gaming-Desktop-TG02-1xxx 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:58:00 up 14:01,  1 user,  load average: 1.30, 0.78, 0.55

--- npc/core/src/main/scala/jzcore/JzCore.scala
+++ npc/core/src/main/scala/jzcore/JzCore.scala
@@ -195,7 +195,7 @@ class JzCore extends Module {
   val forward = Module(new Forwarding)
 
   clint.io.clintIO    <> lsu.io.clintIO
-  idu.io.timerInt     <> csr.io.timerInt
+  idu.io.timerInt     <> idu.io.timerInt
 
   //io.csrAddr  := idu.io.csrAddr
   // 仲裁
