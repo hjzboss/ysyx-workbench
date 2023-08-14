f7494734c04962848f1ba2f8fc2e1a105e94746a
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-14 13:58:17 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:58:17 up 18:27,  2 users,  load average: 0.08, 0.08, 0.02

--- npc/core/src/main/scala/jzcore/FU/Divider.scala
+++ npc/core/src/main/scala/jzcore/FU/Divider.scala
@@ -4,30 +4,6 @@ import chisel3._
 import chisel3.util._
 import utils._
 
-/*
-// 加减交替法
-class Divider extends Module {
-  val io = IO(new Bundle {
-    val flush   = Input(Bool())
-    val in      = Flipped(Decoupled(DivInput))
-    val out     = Decoupled(DivOutput)
-  })
-
-  val inFire = io.in.valid & io.in.ready
-  val outFire = io.out.valid & io.out.ready
-
-  val idle :: busy :: ok :: Nil = Enum(3)
-  val state = RegInit(idle)
-  state := MuxLookup(
-    idle -> Mux(inFire && !io.flush, busy, idle),
-    busy -> Mux(),
-    ok   -> Mux(outFire || io.in.flush, idle, ok)
-  )
-
-  
-}
-*/
-
 // 恢复余数法
 class Divider(len: Int) extends Module {
   val io = IO(new Bundle {
@@ -40,13 +16,12 @@ class Divider(len: Int) extends Module {
   def getN(num: UInt): UInt = ~num + 1.U
 
   val done = WireDefault(false.B) // 除法结束标志
-  val inFire = io.in.valid & io.in.ready
   val outFire = io.out.valid & io.out.ready
 
   val idle :: compute :: recover :: ok :: Nil = Enum(4)
   val state = RegInit(idle)
   state := MuxLookup(state, idle, List(
-    idle    -> Mux(inFire && !io.flush, compute, idle),
+    idle    -> Mux(io.in.valid && !io.flush, compute, idle),
     compute -> Mux(io.flush, idle, Mux(done, recover, compute)),
     recover -> Mux(io.flush, idle, ok),
     ok      -> Mux(io.flush || outFire, idle, ok)
@@ -66,11 +41,11 @@ class Divider(len: Int) extends Module {
     val tmp     = WireDefault(0.U((len/2+1).W))
     done       := cnt === (len/2-1).U
     neg        := io.in.dividend(len/2-1) ^ io.in.divisor(len/2-1)
-    divisor    := Mux(state === idle && inFire, Mux(io.in.divisor(len/2-1) && io.in.divSigned, getN(io.in.divisor), io.in.divisor), divisor)
+    divisor    := Mux(state === idle && io.in.valid, Mux(io.in.divisor(len/2-1) && io.in.divSigned, getN(io.in.divisor), io.in.divisor), divisor)
 
-    when(state === idle && inFire) {
+    when(state === idle && io.in.valid) {
       quotient  := 0.U(len.W)
-      dividend := Mux(io.in.dividend(len/2-1) && io.in.divSigned, ZeroExt(getN(io.in.dividend(len/2-1, 0)), 2*len), ZeroExt(io.in.dividend(len/2-1, 0), 2*len))
+      dividend  := Mux(io.in.dividend(len/2-1) && io.in.divSigned, ZeroExt(getN(io.in.dividend(len/2-1, 0)), 2*len), ZeroExt(io.in.dividend(len/2-1, 0), 2*len))
     }.elsewhen(state === compute) {
       tmp := dividend(len-1, len/2-1) - (0.U(1.W) ## divisor(len/2-1, 0))
       when(tmp(len/2)) {
@@ -106,9 +81,9 @@ class Divider(len: Int) extends Module {
     val tmp     = WireDefault(0.U((len+1).W)) // 中间相减的结果
     done       := cnt === (len-1).U
     neg        := io.in.dividend(len-1) ^ io.in.divisor(len-1)
-    divisor    := Mux(state === idle && inFire, Mux(io.in.divisor(len-1) && io.in.divSigned, getN(io.in.divisor), io.in.divisor), divisor)
+    divisor    := Mux(state === idle && io.in.valid, Mux(io.in.divisor(len-1) && io.in.divSigned, getN(io.in.divisor), io.in.divisor), divisor)
 
-    when(state === idle && inFire) {
+    when(state === idle && io.in.valid) {
       quotient  := 0.U(len.W)
       // 取绝对值
       dividend := Mux(io.in.dividend(len-1) && io.in.divSigned, 0.U(len.W) ## getN(io.in.dividend), 0.U(len.W) ## io.in.dividend)
@@ -116,11 +91,11 @@ class Divider(len: Int) extends Module {
       tmp := dividend(2*len-1, len-1) - (0.U(1.W) ## divisor)
       when(tmp(len)) {
         // 为负数被除数保持不变, 商0
-        dividend := dividend ## 0.U(1.W)
+        dividend  := dividend ## 0.U(1.W)
         quotient  := quotient(len-2, 0) ## 0.U(1.W)
       }.otherwise {
         val remTmp = tmp ## dividend(len-2, 0)
-        dividend := remTmp(2*len-2, 0) ## 0.U(1.W) // 左移一位
+        dividend  := remTmp(2*len-2, 0) ## 0.U(1.W) // 左移一位
         quotient  := quotient(len-2, 0) ## 1.U(1.W)
       }
     }.elsewhen(state === recover && io.in.divSigned) {
@@ -144,7 +119,7 @@ class Divider(len: Int) extends Module {
       remainder := remainder
     }
 
-    io.out.bits.quotient := quotient
+    io.out.bits.quotient  := quotient
     io.out.bits.remainder := remainder
   }
 }
