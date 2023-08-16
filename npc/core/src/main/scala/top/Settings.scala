c215813abf02abd7924d12456883d4eeb4dd1b52
tracer-ysyx2204 <tracer@ysyx.org>
2023-08-16 19:54:18 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:54:18 up 10:16,  2 users,  load average: 0.15, 0.15, 0.34

--- npc/core/src/main/scala/top/Settings.scala
+++ npc/core/src/main/scala/top/Settings.scala
@@ -8,7 +8,7 @@ object DefaultSettings {
     "ResetVector" -> 0x80000000L,
     "TestVector"  -> 0x00000000L,
     "SocResetVector" -> 0x30000000L,
-    "lowpower"    -> true
+    "lowpower"    -> false
   )
 }
 
