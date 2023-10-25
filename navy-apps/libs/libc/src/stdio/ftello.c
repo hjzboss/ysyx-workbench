d14b3a19f1d395fd6030b93b6c119f097d23f2ac
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-25 10:56:47 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:56:47 up 5 days, 13:44,  2 users,  load average: 0.43, 0.48, 0.32

--- navy-apps/libs/libc/src/stdio/ftello.c
+++ navy-apps/libs/libc/src/stdio/ftello.c
@@ -106,7 +106,6 @@ _ftello_r (struct _reent * ptr,
       fp->_p != NULL && fp->_p - fp->_bf._base > 0 &&
       (fp->_flags & __SAPP))
     {
-      printf("shit1\n");
       pos = fp->_seek (ptr, fp->_cookie, (_fpos_t) 0, SEEK_END);
       if (pos == (_fpos_t) -1)
 	{
@@ -134,8 +133,8 @@ _ftello_r (struct _reent * ptr,
        * those from ungetc) cause the position to be
        * smaller than that in the underlying object.
        */
+      printf("shit1\n");
       pos -= fp->_r;
-      printf("fuck\n");
       if (HASUB (fp))
 	pos -= fp->_ur;
     }
@@ -146,6 +145,7 @@ _ftello_r (struct _reent * ptr,
        * position to be greater than that in the
        * underlying object.
        */
+      printf("shit2\n");
       pos += fp->_p - fp->_bf._base;
     }
 
