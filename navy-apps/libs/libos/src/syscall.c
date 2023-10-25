66ab143c5259237d66fbb923bd5670c28ece1ea4
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-25 15:42:48 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:42:48 up 5 days, 18:30,  2 users,  load average: 0.51, 1.06, 0.80

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -92,10 +92,10 @@ int _close(int fd) {
 off_t _lseek(int fd, off_t offset, int whence) {
   off_t ret = _syscall_(SYS_lseek, fd, offset, whence);
   asm volatile(
-    "mv s0, %[ret]\n"
+    "mv s1, %[ret]\n"
     :
     : [ret] "r" (ret)
-    : "s0"
+    : "s1"
   );
   return ret;
 }
