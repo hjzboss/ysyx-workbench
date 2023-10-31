e468ed5cd23db5ea8f6a0c980c6417070ff53a64
tracer-ysyx2204 <tracer@ysyx.org>
2023-10-31 18:44:16 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:44:16 up 23:18,  2 users,  load average: 0.45, 0.37, 0.18

--- npc/core/src/main/scala/top/Settings.scala
+++ npc/core/src/main/scala/top/Settings.scala
@@ -12,10 +12,10 @@ object DefaultSettings {
     "ResetVector" -> 0x80000000L,
     "TestVector"  -> 0x00000000L,
     "SocResetVector" -> 0x30000000L,
-    "mul"         -> "fast", // fast, booth, wallance
-    "div"         -> "fast", // fast, rest
+    "mul"         -> "wallance", // fast, booth, wallance
+    "div"         -> "rest", // fast, rest
     "sim"         -> true, // verilator mode， false is soc mode， no debug
-    "core"        -> "single" // single, fast, normal
+    "core"        -> "fast" // single, fast, normal
   )
 }
 
