f142c20dec1436562cc4d1e9642e01c6c734ed30
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-01 11:17:25 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:17:25 up 1 day,  3:28,  2 users,  load average: 0.02, 0.09, 0.16

--- npc/core/src/main/scala/jzcore/CORE/EXU.scala
+++ npc/core/src/main/scala/jzcore/CORE/EXU.scala
@@ -81,7 +81,7 @@ class EXU extends Module {
   io.out.lsuWen        := io.ctrl.memWen
   io.out.lsuRen        := io.ctrl.memRen
   io.out.lsuAddr       := aluOut(31, 0)
-  io.out.lsuWdata      := opB
+  io.out.lsuWdata      := opBPre
   io.out.loadMem       := io.ctrl.loadMem
 
   // exu output
