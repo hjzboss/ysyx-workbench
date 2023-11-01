a03db9e33bf88c19d16b99286cdfd195542a8c5d
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-01 15:49:16 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:49:16 up 1 day,  8:00,  2 users,  load average: 0.76, 0.21, 0.08

--- npc/core/src/main/scala/jzcore/CTRL/CTRL.scala
+++ npc/core/src/main/scala/jzcore/CTRL/CTRL.scala
@@ -42,11 +42,13 @@ class CTRL extends Module {
   })
 
   val loadUse     = dontTouch(WireDefault(false.B))
-  loadUse        := io.memRen && (io.exRd === io.rs1 || io.exRd === io.rs2) 
+  loadUse        := io.memRen && (io.exRd === io.rs1 || io.exRd === io.rs2)
 
-  // 当取指未完成时停顿之前所有阶段，当前面指令有csr操作是时候停顿后面阶段的指令
+  val branch      = io.branch & ~io.brUse // 当出现brUse时说明操作数还没准备好，并不是真正的跳转有效
+
+  // 当取指未完成时停顿之前所有阶段，当前面指令有csr操作是时候停顿后面阶段pc的指令
   io.stallICache := !io.lsuReady | loadUse | !io.exuReady | io.exuCsr | io.lsuCsr | io.wbuCsr | io.brUse
-  io.stallPc     := !io.lsuReady | loadUse | (io.icStall & !io.branch) | !io.exuReady | io.exuCsr | io.lsuCsr | io.wbuCsr | io.brUse
+  io.stallPc     := !io.lsuReady | loadUse | (io.icStall & !branch) | !io.exuReady | io.exuCsr | io.lsuCsr | io.wbuCsr | io.brUse
   io.stallIduReg := !io.lsuReady | loadUse | !io.exuReady | io.exuCsr | io.lsuCsr | io.wbuCsr | io.brUse
   io.stallExuReg := !io.lsuReady | !io.exuReady
   io.stallLsuReg := !io.lsuReady | !io.exuReady
@@ -54,8 +56,8 @@ class CTRL extends Module {
   io.stallExu    := !io.lsuReady
 
   // 当取指未完成或者发现是分支指令时flush idu_reg
-  io.flushICache := io.branch
-  io.flushIduReg := io.branch
+  io.flushICache := branch
+  io.flushIduReg := branch
   io.flushExuReg := loadUse | io.exuCsr | io.lsuCsr | io.wbuCsr | io.brUse
 }
 
