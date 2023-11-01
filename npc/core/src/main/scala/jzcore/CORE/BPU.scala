fd483a3141b09e143261b2c5dd994e0cf3fb2fc7
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-01 16:05:03 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:03 up 1 day,  8:16,  2 users,  load average: 0.03, 0.36, 0.43

---
+++ npc/core/src/main/scala/jzcore/CORE/BPU.scala
@@ -0,0 +1,14 @@
+package jzcore
+
+import chisel3._
+import top.Settings
+import chisel3.util._
+
+
+// 分支预测模块
+class BPU extends Module with HasResetVector {
+  val io = IO(new Bundle{
+
+  })
+  
+}
