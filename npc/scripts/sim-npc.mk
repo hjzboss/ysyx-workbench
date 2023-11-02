a28a24ac1b04114b2c096ff0dd0d94e3607a5e62
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-02 10:20:53 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:53 up 41 min,  1 user,  load average: 0.00, 0.17, 0.30

--- npc/scripts/sim-npc.mk
+++ npc/scripts/sim-npc.mk
@@ -45,6 +45,7 @@ sim: $(SIM_CSRC) $(VSRC)
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
 	@rm -rf $(SIM_OBJ_DIR)
 	@echo "build"
+	@echo $(IMAGE_OBJ)
 	$(VERILATOR) $(VERILATOR_SIMFLAG) $^
 	$(SIM_OBJ_DIR)/V${USER_ID}_$(TOPNAME) $(NPC_FLAG)
 	@echo "wave"
