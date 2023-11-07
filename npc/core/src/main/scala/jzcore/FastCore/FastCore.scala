63ed9bc460d3ba71ec7910d6bd9ecb1a05f812bc
tracer-ysyx2204 <tracer@ysyx.org>
2023-11-07 15:14:51 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:51 up 1 day,  2:35,  2 users,  load average: 0.37, 0.17, 0.05

--- npc/core/src/main/scala/jzcore/FastCore/FastCore.scala
+++ npc/core/src/main/scala/jzcore/FastCore/FastCore.scala
@@ -86,7 +86,7 @@ class FastCore extends Module {
   idu.io.lsuForward := lsReg.io.out.exuOut
   idu.io.wbuForward := wbu.io.regWrite.value
   idu.io.icPc       := ifu.io.out.pc
-  idu.io.icValid    := ifu.io.out.valid
+  idu.io.icValid    := ifu.io.valid
 
   exu.io.datasrc    <> exReg.io.datasrcOut
   exu.io.aluCtrl    <> exReg.io.aluCtrlOut
