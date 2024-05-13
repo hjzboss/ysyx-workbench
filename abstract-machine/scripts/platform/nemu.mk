57fcdd779917e05278fda5c33ccdc6aa8ab65b38
tracer-ysyx2204 <tracer@ysyx.org>
2024-05-13 17:02:46 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:46 up 5 days,  9:56,  1 user,  load average: 0.09, 0.22, 0.14

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -16,7 +16,7 @@ NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += -e ${IMAGE}.elf
 
 # batch
-#NEMUFLAGS += -b
+NEMUFLAGS += -b
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
