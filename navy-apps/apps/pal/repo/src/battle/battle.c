0f99780af8e2ad4a4fd1eb11928c3dd3e0da3582
tracer-ysyx2204 <tracer@ysyx.org>
2024-06-20 17:11:13 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:11:13 up 16:46,  1 user,  load average: 0.51, 0.25, 0.21

--- navy-apps/apps/pal/repo/src/battle/battle.c
+++ navy-apps/apps/pal/repo/src/battle/battle.c
@@ -1320,7 +1320,8 @@ PAL_StartBattle(
    //
    for (i = 0; i <= gpGlobals->wMaxPartyMemberIndex; i++)
    {
-      g_Battle.rgPlayer[i].flTimeMeter = FLOATfromInt(100);
+      // 将时间直接跳到99，快速进入战斗画面
+      g_Battle.rgPlayer[i].flTimeMeter = FLOATfromInt(99);
 #ifndef PAL_CLASSIC
       g_Battle.rgPlayer[i].flTimeSpeedModifier = 2;
       g_Battle.rgPlayer[i].sTurnOrder = -1;
