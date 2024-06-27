b3def95429c71761462e62db53ecc5c5d315e09a
tracer-ysyx2204 <tracer@ysyx.org>
2024-06-27 22:05:26 +0800

    > sim RTL ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:05:26 up 1 day,  4:21,  1 user,  load average: 0.36, 0.58, 0.75

--- npc/csrc/device/device.cpp
+++ npc/csrc/device/device.cpp
@@ -5,30 +5,33 @@
 void init_vga();
 void vga_update_screen();
 void init_i8042();
-
-
 void send_key(uint8_t, bool);
-void vga_update_screen();
+
+uint64_t cnt = 0;
 
 void device_update() {
+  cnt++;
   vga_update_screen();
 
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        npc_state.state = NPC_QUIT;
-        break;
-      // If a key was pressed
-      case SDL_KEYDOWN:
-      case SDL_KEYUP: {
-        uint8_t k = event.key.keysym.scancode;
-        bool is_keydown = (event.key.type == SDL_KEYDOWN);
-        send_key(k, is_keydown);
-        break;
+  if(cnt > 10000) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          npc_state.state = NPC_QUIT;
+          break;
+        // If a key was pressed
+        case SDL_KEYDOWN:
+        case SDL_KEYUP: {
+          uint8_t k = event.key.keysym.scancode;
+          bool is_keydown = (event.key.type == SDL_KEYDOWN);
+          send_key(k, is_keydown);
+          break;
+        }
+        default: break;
       }
-      default: break;
     }
+    cnt = 0;
   }
 }
 
