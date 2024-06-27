e0494b43defdcfdba9d33b4506f20f59ebd9f0f8
tracer-ysyx2204 <tracer@ysyx.org>
2024-06-27 17:18:16 +0800

    > compile NEMU ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:18:16 up 1 day,  2:05,  1 user,  load average: 0.12, 0.16, 0.17

--- am-kernels/tests/cpu-tests/tests/mulw.c
+++ am-kernels/tests/cpu-tests/tests/mulw.c
@@ -3,9 +3,9 @@
 
 
 int main() {
-  uint64_t a = 234;
-  uint64_t b = 123131;
-  uint32_t c = (uint32_t)a * (uint32_t)b;
+  //uint64_t a = 234;
+  //uint64_t b = 123131;
+  //uint32_t c = (uint32_t)a * (uint32_t)b;
 
-  printf("%d\n", c);
+  printf("%d\n", 123);
 }
