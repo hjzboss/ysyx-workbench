15f3cb863a88f6108acf67ed2d8bb61bf4327ccd
tracer-ysyx2204 <tracer@ysyx.org>
2024-06-30 15:29:47 +0800

    > generate verilog ysyx_22050853 黄俊哲 Linux hjz-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:29:47 up 1 day,  5:13,  1 user,  load average: 1.00, 0.59, 0.25

--- npc/scripts/sim-npc.mk
+++ npc/scripts/sim-npc.mk
@@ -1,73 +1,86 @@
-BUILD_DIR = ${NPC_HOME}/build
+# npc构建目录
+NPC_BUILD_DIR = ${NPC_HOME}/build
 
+# chisel blackbox代码目录
 BLACKBOX_DIR = ${NPC_HOME}/core/src/main/verilog
 
+# ysyx id
 USER_ID = ysyx_22050853
 
+# chisel生成的顶层文件名
 TOPNAME = Soc
 
-VSRC = ${BUILD_DIR}/${TOPNAME}.v
-VSRC += $(shell find $(abspath ${BLACKBOX_DIR}) -name "*.sv")
+# verilog source
+VSRC_NPC = ${NPC_BUILD_DIR}/${TOPNAME}.v
+VSRC_NPC += $(shell find $(abspath ${BLACKBOX_DIR}) -name "*.sv")
 
-SIM_CSRC = $(shell find $(abspath ${NPC_HOME}/csrc) -name "*.cpp")
+# cpp source
+SIM_CSRC_NPC = $(shell find $(abspath ${NPC_HOME}/csrc) -name "*.cpp")
 
-VERILATOR = verilator
-SIM_OBJ_DIR = $(BUILD_DIR)/sim/obj_dir
+# npc仿真文件目录
+NPC_SIM_OBJ_DIR = $(NPC_BUILD_DIR)/sim/obj_dir
+
+# 波形文件名称
 WAVE_OBJ = wave.vcd
 
+# 加载的镜像文件
+IMAGE_OBJ ?= 
+
 # npc仿真参数
-NPC_FLAG += -l $(BUILD_DIR)/npc-log.txt
+# log文件地址
+NPC_FLAG += -l $(NPC_BUILD_DIR)/npc-log.txt
+# 所加载的镜像文件
 NPC_FLAG += -i $(IMAGE_OBJ)
+# elf文件，用于etrace
 NPC_FLAG += -e ${IMAGE}.elf
+# 批处理模式
 NPC_FLAG += -b
 
 # difftest动态库
 DIFFSET_SO := ${NEMU_HOME}/build/riscv64-nemu-interpreter-so
 
-# 链接参数
-LFLAGS += $(shell llvm-config --libs) -lreadline -ldl -pie -lSDL2
+# npc链接参数
+LFLAGS_NPC += $(shell llvm-config --libs) -lreadline -ldl -pie -lSDL2
 
-# c参数
-CFLAGS += -I${NPC_HOME}/include -O2 -I/usr/lib/llvm-14/include -fno-exceptions -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
+# npc c参数
+CFLAGS_NPC += -I${NPC_HOME}/include -O2 -I/usr/lib/llvm-14/include -std=c++14 -fno-exceptions -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_LIMIT_MACROS
 
 # difftest
 ifneq ($(DIFF),)
 NPC_FLAG += -d ${DIFFSET_SO}
-LFLAGS += $(DIFFSET_SO)
-CFLAGS += -DCONFIG_DIFFTEST=1
+LFLAGS_NPC += $(DIFFSET_SO)
+CFLAGS_NPC += -DCONFIG_DIFFTEST=1
 endif
 
 # wave
 ifneq ($(WAVE),)
-CFLAGS += -DCONFIG_WAVE=1
+CFLAGS_NPC += -DCONFIG_WAVE=1
 endif
 
 # verilator仿真参数
-VERILATOR_SIMFLAG = 
+VERILATOR_SIMFLAG_NPC = 
 # build
-VERILATOR_SIMFLAG += --cc --exe --build -MMD
+VERILATOR_SIMFLAG_NPC += --cc --exe --build -MMD
 # C++ compiler arguments for makefile
-VERILATOR_SIMFLAG += -CFLAGS "${CFLAGS}"
+VERILATOR_SIMFLAG_NPC += -CFLAGS "${CFLAGS_NPC}"
 # open trace
-VERILATOR_SIMFLAG += --trace --Mdir $(SIM_OBJ_DIR)
+VERILATOR_SIMFLAG_NPC += --trace --Mdir $(SIM_OBJ_DIR)
 # top module
-VERILATOR_SIMFLAG += --top-module ${USER_ID}_$(TOPNAME)
-
-IMAGE_OBJ ?= 
-
-VERILATOR_SIMFLAG += -LDFLAGS "$(LFLAGS)"
+VERILATOR_SIMFLAG_NPC += --top-module ${USER_ID}_$(TOPNAME)
+# 链接
+VERILATOR_SIMFLAG_NPC += -LDFLAGS "$(LFLAGS_NPC)"
 
 # 仿真
 sim: 
 	make -C ${NPC_HOME} verilog
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
-	@rm -rf $(SIM_OBJ_DIR)
-	@echo "build"
-	$(VERILATOR) $(VERILATOR_SIMFLAG) $(SIM_CSRC) $(VSRC)
-	$(SIM_OBJ_DIR)/V${USER_ID}_$(TOPNAME) $(NPC_FLAG)
+	@rm -rf $(NPC_SIM_OBJ_DIR)
+	@echo "build npc verilator"
+	verilator $(VERILATOR_SIMFLAG_NPC) $(SIM_CSRC_NPC) $(VSRC_NPC)
+	$(NPC_SIM_OBJ_DIR)/V${USER_ID}_$(TOPNAME) $(NPC_FLAG)
 ifneq ($(WAVE),)
 	@echo "wave"
-	gtkwave $(SIM_OBJ_DIR)/$(WAVE_OBJ)
+	gtkwave $(NPC_SIM_OBJ_DIR)/$(WAVE_OBJ)
 endif
 
 # 查看波形
